51021e79185e6767e35ae100722df517642f13fa
Eamon Gordon <ekeokigordon@icloud.com>
2023-12-15 08:55:09 -0800

    added nextui

---
+++ app/providers.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+//import { SessionProvider } from "next-auth/react";
+import { NextUIProvider } from '@nextui-org/react';
+//import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    //<SessionProvider>
+      <NextUIProvider>
+        {children}
+      </NextUIProvider>
+   // </SessionProvider>
+  );
+}
