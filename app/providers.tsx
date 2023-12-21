0ff9543cbc9886f14d57a3978d3fde7e12d6e5c3
Eamon Gordon <ekeokigordon@icloud.com>
2023-12-20 23:37:43 -0800

    add sonnet toaster

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import { Toaster } from "sonner";
 import { NextUIProvider } from '@nextui-org/react';
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
@@ -9,6 +10,8 @@ export function Providers({ children }: { children: React.ReactNode }) {
     <SessionProvider>
       <NextUIProvider>
         <NextThemesProvider attribute="class">
+          <Toaster className="dark:hidden" />
+          <Toaster theme="dark" className="hidden dark:block" />
           {children}
         </NextThemesProvider>
       </NextUIProvider>
