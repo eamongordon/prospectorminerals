2bf17e7c4eee7d0fc68a64afa342e7816776bd9f
Eamon Gordon <ekeokigordon@icloud.com>
2023-12-20 18:47:04 -0800

    add google login

--- app/login/login-button.tsx
+++ app/login/login-button.tsx
@@ -3,6 +3,7 @@
 import LoadingDots from "@/components/icons/loading-dots";
 import { signIn } from "next-auth/react";
 import { useSearchParams } from "next/navigation";
+import { Button } from "@nextui-org/react";
 import { useState, useEffect } from "react";
 import { toast } from "sonner";
 
@@ -19,35 +20,53 @@ export default function LoginButton() {
   }, [error]);
 
   return (
-    <button
+    <Button
       disabled={loading}
       onClick={() => {
         setLoading(true);
-        signIn("github");
+        signIn("google");
       }}
-      className={`${
-        loading
-          ? "cursor-not-allowed bg-stone-50 dark:bg-stone-800"
-          : "bg-white hover:bg-stone-50 active:bg-stone-100 dark:bg-black dark:hover:border-white dark:hover:bg-black"
-      } group my-2 flex h-10 w-full items-center justify-center space-x-2 rounded-md border border-stone-200 transition-colors duration-75 focus:outline-none dark:border-stone-700`}
+      variant="flat"
+      className="flex w-full items-center"
+      startContent={
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 256 262"><path className="fill-black dark:fill-white" d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622l38.755 30.023l2.685.268c24.659-22.774 38.875-56.282 38.875-96.027" /><path className="fill-black dark:fill-white" d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055c-34.523 0-63.824-22.773-74.269-54.25l-1.531.13l-40.298 31.187l-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1" /><path className="fill-black dark:fill-white" d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82c0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602z" /><path className="fill-black dark:fill-white" d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0C79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251" /></svg>
+      }
     >
-      {loading ? (
-        <LoadingDots color="#A8A29E" />
-      ) : (
-        <>
-          <svg
-            className="h-4 w-4 text-black dark:text-white"
-            aria-hidden="true"
-            fill="currentColor"
-            viewBox="0 0 24 24"
-          >
-            <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-          </svg>
-          <p className="text-sm font-medium text-stone-600 dark:text-stone-400">
-            Login with GitHub
-          </p>
-        </>
-      )}
-    </button>
-  );
+      Sign In with Google
+    </Button>
+  )
+  /*
+    return (
+      <button
+        disabled={loading}
+        onClick={() => {
+          setLoading(true);
+          signIn("github");
+        }}
+        className={`${
+          loading
+            ? "cursor-not-allowed bg-stone-50 dark:bg-stone-800"
+            : "bg-white hover:bg-stone-50 active:bg-stone-100 dark:bg-black dark:hover:border-white dark:hover:bg-black"
+        } group my-2 flex h-10 w-full items-center justify-center space-x-2 rounded-md border border-stone-200 transition-colors duration-75 focus:outline-none dark:border-stone-700`}
+      >
+        {loading ? (
+          <LoadingDots color="#A8A29E" />
+        ) : (
+          <>
+            <svg
+              className="h-4 w-4 text-black dark:text-white"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+            </svg>
+            <p className="text-sm font-medium text-stone-600 dark:text-stone-400">
+              Login with GitHub
+            </p>
+          </>
+        )}
+      </button>
+    );
+    */
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,8 +3,10 @@ import LoginButton from "./login-button";
 import { Suspense } from "react";
 import Form from "@/components/login-form";
 import Link from "next/link";
+import { Divider } from "@nextui-org/react";
 
 export default function LoginPage() {
+  /*
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-gray-50">
       <div className="z-10 w-full max-w-md overflow-hidden rounded-2xl border border-gray-100 shadow-xl">
@@ -28,41 +30,32 @@ export default function LoginPage() {
       </div>
     </div>
   );
-  /*
+*/
   return (
-    <div className="mx-5 border border-stone-200 py-10 dark:border-stone-700 sm:mx-auto sm:w-full sm:max-w-md sm:rounded-lg sm:shadow-md">
-      <Image
-        alt="Platforms Starter Kit"
-        width={100}
-        height={100}
-        className="relative mx-auto h-12 w-auto dark:scale-120 dark:rounded-full dark:border dark:border-stone-400"
-        src="/PM-Favicon-New-Square.svg"
-      />
-      <h1 className="mt-6 text-center font-cal text-3xl dark:text-white">
-        Welcome Back
-      </h1>
-      <p className="mt-2 text-center text-sm text-stone-600 dark:text-stone-400">
-        Don&apos;t have an account? <br />
-        <a
-          className="font-medium text-black hover:text-stone-800 dark:text-stone-300 dark:hover:text-stone-100"
-          href="https://vercel.com/blog/platforms-starter-kit"
-          rel="noreferrer"
-          target="_blank"
-        >
-          Sign Up
-        </a>
-      </p>
-
-      <div className="mx-auto mt-4 w-11/12 max-w-xs sm:w-full">
-        <Suspense
-          fallback={
-            <div className="my-2 h-10 w-full rounded-md border border-stone-200 bg-stone-100 dark:border-stone-700 dark:bg-stone-800" />
-          }
-        >
-          <LoginButton />
-        </Suspense>
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className="mx-5 border border-stone-200 py-10 dark:border-stone-700 sm:mx-auto sm:w-full sm:max-w-md sm:rounded-lg sm:shadow-md">
+        <Image
+          alt="Platforms Starter Kit"
+          width={100}
+          height={100}
+          className="relative mx-auto h-12 w-auto dark:scale-120 dark:rounded-full dark:border dark:border-stone-400"
+          src="/PM-Favicon-New-Square.svg"
+        />
+        <h1 className="mt-6 text-center font-medium text-3xl dark:text-white">
+          Welcome Back
+        </h1>
+        <Form type="login" />
+        <Divider/>
+        <div className="mx-auto mt-8 w-11/12 max-w-xs sm:w-full">
+          <Suspense
+            fallback={
+              <div className="my-2 h-10 w-full rounded-md border border-stone-200 bg-stone-100 dark:border-stone-700 dark:bg-stone-800" />
+            }
+          >
+            <LoginButton />
+          </Suspense>
+        </div>
       </div>
     </div>
   );
-  */
 }
