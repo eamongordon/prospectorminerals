bab8ef4f6be046a6b56727509f72c76e1f7e3e9c
Eamon Gordon <ekeokigordon@icloud.com>
2023-12-22 15:40:53 -0800

    mac fkl

---
+++ components/registration/login-modal-button.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useModal } from "@/components/modal/provider";
+import { ReactNode } from "react";
+import { Button } from "@nextui-org/react";
+
+export default function LoginModalButton({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const modal = useModal();
+  return (
+    <Button
+      onClick={() => modal?.show(children)}
+      color="default" variant="flat" className="hidden sm:flex"
+    >
+      Create New Site
+    </Button>
+  );
+}
