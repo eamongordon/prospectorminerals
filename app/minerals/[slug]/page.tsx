971832bf0718611b45c9d99fc33e263be538b6f6
Eamon Gordon <ekeokigordon@icloud.com>
2023-12-24 15:07:46 -0800

    add support for other modal

--- app/minerals/[slug]/page.tsx
+++ app/minerals/[slug]/page.tsx
@@ -3,15 +3,17 @@ import Header from '@/components/header';
 import Footer from '@/components/footer';
 import LoginModalButton from '@/components/registration/login-modal-button';
 import LoginModal from '@/components/modal/login';
-import LoginForm from '@/components/registration/login-form';
-import Modal from "@/components/next-ui-modal";
+//import LoginForm from '@/components/registration/login-form';
+//import Modal from "@/components/next-ui-modal";
 
 export default function Page({ params }: { params: { slug: string } }) {
     return (
         <main>
             <Header />
             <div>My Post: {params.slug}</div>
-            <Modal />
+            <LoginModalButton>
+                <LoginModal />
+            </LoginModalButton>
             <Footer />
         </main>
     )
