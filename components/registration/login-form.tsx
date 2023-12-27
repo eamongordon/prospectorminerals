799b711cca224b81f102f144a31a7044291c12bc
Eamon Gordon <ekeokigordon@icloud.com>
2023-12-26 16:20:28 -0800

    add reset password function

--- components/registration/login-form.tsx
+++ components/registration/login-form.tsx
@@ -5,14 +5,27 @@ import { useSearchParams } from 'next/navigation'
 import Image from "next/image";
 import LoginButton from "./social-login-button";
 import { Suspense } from "react";
-import { Divider, Tab, Tabs, Link } from "@nextui-org/react";
+import { Divider, Tab, Tabs, Link, Spacer } from "@nextui-org/react";
 import React from "react";
 
-export default function FormWrapper({ isModal, onCloseAction }: { isModal?: boolean, onCloseAction?: Function }) {
+export default function FormWrapper(
+  {
+    isModal,
+    onCloseAction
+  }: {
+    isModal?: boolean,
+    onCloseAction?: Function
+
+  }) {
   const router = useRouter();
   const searchParams = useSearchParams()
   const redirectUri = searchParams.get('redirect');
   const [selected, setSelected] = React.useState("/login");
+  const [forgotPassword, setForgotPassword] = React.useState(false);
+  const pull_ForgotPassword = (data: string) => {
+    console.log(data)
+    setForgotPassword(true);
+  }
   const pathname = usePathname();
   return (
     <div className={`${isModal ? "flex items-center justify-center" : "flex h-screen w-screen items-center justify-center"}`}>
@@ -37,27 +50,45 @@ export default function FormWrapper({ isModal, onCloseAction }: { isModal?: bool
               className="relative mx-auto h-12 w-auto dark:scale-120 dark:rounded-full dark:border dark:border-stone-400 my-5"
               src="/PM-Favicon-New-Square.svg"
             />
-            <h1 className="mt-6 text-center font-medium text-3xl dark:text-white">
-              Welcome Back
-            </h1>
-            <Form type="login" isModal={isModal} onCloseAction={onCloseAction} />
-            <p className="text-center text-sm pt-8 pb-8 px-16">
-              Don&apos;t have an account?{" "}
-              <Link className="font-semibold text-sm" color="foreground" {...(isModal ? { onPress: () => setSelected("/signup") } : { href: "/signup" })} >
-                Sign up
-              </Link>{" "}
-              for free.
-            </p>
-            <Divider />
-            <div className="flex flex-col space-y-4 px-4 mt-8 mb-8 sm:px-16">
-              <Suspense
-                fallback={
-                  <div className="my-2 h-10 w-full rounded-md border border-stone-200 bg-stone-100 dark:border-stone-700 dark:bg-stone-800" />
-                }
-              >
-                <LoginButton signup={false} />
-              </Suspense>
-            </div>
+            {
+              forgotPassword ? (
+                <>
+                  <h1 className="mt-6 text-center font-medium text-3xl dark:text-white">
+                    Reset Password
+                  </h1>
+                  <p className="text-center text-sm pt-4 px-16">
+                    Send a login link to your account's email.
+                  </p>
+                  <div className='pb-8'>
+                  <Form type="forgotPassword" isModal={isModal} onCloseAction={onCloseAction} resetPasswordFunc={pull_ForgotPassword} />
+                  </div>
+                </>
+              ) : (
+                <>
+                  <h1 className="mt-6 text-center font-medium text-3xl dark:text-white">
+                    Welcome Back
+                  </h1>
+                  <Form type="login" isModal={isModal} onCloseAction={onCloseAction} resetPasswordFunc={pull_ForgotPassword} />
+                  <p className="text-center text-sm pt-8 pb-8 px-16">
+                    Don&apos;t have an account?{" "}
+                    <Link className="font-semibold text-sm" color="foreground" {...(isModal ? { onPress: () => setSelected("/signup") } : { href: "/signup" })} >
+                      Sign up
+                    </Link>{" "}
+                    for free.
+                  </p>
+                  <Divider />
+                  <div className="flex flex-col space-y-4 px-4 mt-8 mb-8 sm:px-16">
+                    <Suspense
+                      fallback={
+                        <div className="my-2 h-10 w-full rounded-md border border-stone-200 bg-stone-100 dark:border-stone-700 dark:bg-stone-800" />
+                      }
+                    >
+                      <LoginButton signup={false} />
+                    </Suspense>
+                  </div>
+                </>
+              )
+            }
           </Tab>
           <Tab key="/signup" title="Sign Up" {...(isModal ? {} : { href: "/signup" })}>
             <Image
@@ -92,7 +123,7 @@ export default function FormWrapper({ isModal, onCloseAction }: { isModal?: bool
           </Tab>
         </Tabs>
       </div>
-    </div>
+    </div >
   )
 
 }
