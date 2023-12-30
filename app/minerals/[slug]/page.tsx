1c44c4022503fc5eb14e70ea370b8740f6a29424
Eamon Gordon <ekeokigordon@icloud.com>
2023-12-29 22:05:03 -0800

    add logo theme NextAuth

--- app/minerals/[slug]/page.tsx
+++ app/minerals/[slug]/page.tsx
@@ -7,15 +7,22 @@ import { LoginModalProvider } from "@/components/modal/registration/provider";
 //import LoginForm from '@/components/registration/login-form';
 //import Modal from "@/components/next-ui-modal";
 import RegModal from "@/components/next-ui-modal";
+import Image from 'next/image';
 
 export default function Page({ params }: { params: { slug: string } }) {
     return (
         <main>
             <LoginModalProvider>
-            <Header />
-            <div>My Post: {params.slug}</div>
-            <RegModal/>
-            <Footer />
+                <Header />
+                <div>My Post: {params.slug}</div>
+                <Image
+                    src="/PM-Favicon-New-Square.png"
+                    width={55}
+                    height={55}
+                    alt="Prospector Minerals Logo"
+                />
+                <RegModal />
+                <Footer />
             </LoginModalProvider>
         </main>
     )
