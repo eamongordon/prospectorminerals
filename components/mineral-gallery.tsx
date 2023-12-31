06b6fc2d455f53d9d5852e384aa49e4c58ec17c7
Eamon Gordon <ekeokigordon@icloud.com>
2023-12-31 13:57:29 -0800

    layout image gallery

--- components/mineral-gallery.tsx
+++ components/mineral-gallery.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Image from 'next/image';
+import {useState} from 'react';
 
 interface galleryItem {
     title?: string;
@@ -6,32 +9,33 @@ interface galleryItem {
 };
 
 export default function Gallery({ data }: { data?: galleryItem[] }) {
+    const [hover, setHover] = useState(false);
     return (
-        <div className="grid grid-rows-2 grid-flow-col gap-4 max-w-screen-lg">
-            <div className="row-span-2 col-span-2">
+        <div className="grid grid-rows-2 grid-cols-3 gap-2 sm:gap-4 max-w-screen-xl aspect-[8/3] max-h-[500px] px-8 mx-auto">
+            <div className="relative rounded-md row-span-2 col-span-2">
                 <Image
-                    src="/NativeCopper-15_horiz.jpeg"
+                    className='rounded-xl'
+                    src="/Amazonite-106_horiz.jpeg"
                     alt="Picture of the author"
-                    height={800}
-                    width={600}
                     objectFit='cover'
+                    fill={true}
                 />
             </div>
-            <div>
+            <div className="relative row-span-1 col-span-1">
                 <Image
+                    className='rounded-xl'
                     src="/Cavansite-45.jpeg"
-                    height={400}
-                    width={600}
+                    fill={true}
                     objectFit='cover'
                     alt="Picture of the author"
                 />
             </div>
-            <div>
+            <div className="relative row-span-1 col-span-1">
                 <Image
+                    className='rounded-xl'
                     src="/Krohnkite-110_horiz.jpeg"
                     alt="Picture of the author"
-                    height={400}
-                    width={600}
+                    fill={true}
                     objectFit='cover'
                 />
             </div>
