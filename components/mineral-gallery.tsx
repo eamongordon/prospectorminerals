39618cbddacc1b65b64de7be5b511eb96937efed
Eamon Gordon <ekeokigordon@icloud.com>
2024-01-01 19:46:37 -0800

    add 1,2,3 gallery item support

--- components/mineral-gallery.tsx
+++ components/mineral-gallery.tsx
@@ -8,7 +8,7 @@ interface galleryItem {
     caption?: string;
 };
 
-export default function Gallery({ data }: { data?: galleryItem[] }) {
+export default function Gallery({ data }: { data: galleryItem[] }) {
     const [hoverItem, setHoverItem] = useState("");
     function handleHoverIn(itemId: string) {
         setHoverItem(itemId);
@@ -17,9 +17,9 @@ export default function Gallery({ data }: { data?: galleryItem[] }) {
         setHoverItem("");
     };
     return (
-        <div className="grid grid-rows-2 grid-cols-3 gap-2 sm:gap-4 max-w-screen-xl aspect-[8/3] max-h-[500px] px-8 mx-auto">
+        <div className={`${data.length >= 3 ? "grid grid-rows-2 grid-cols-3 gap-2 sm:gap-4 max-w-screen-xl aspect-[8/3] max-h-[500px] px-8 mx-auto" : data.length === 2 ? "grid grid-rows-1 grid-cols-2 gap-2 sm:gap-4 max-w-screen-xl aspect-[8/3] max-h-[500px] px-8 mx-auto" : "grid grid-rows-1 grid-cols-1 gap-2 sm:gap-4 max-w-screen-xl aspect-[8/3] max-h-[500px] px-8 mx-auto"}`}>
             <div
-                className="relative rounded-md row-span-2 col-span-2 flex flex-col items-center justify-center"
+                className={`${data.length >= 3 ? "relative rounded-md row-span-2 col-span-2 flex flex-col items-center justify-center" : "relative rounded-md row-span-1 col-span-1 flex flex-col items-center justify-center"}`}
                 id="image1"
                 onMouseEnter={(e) => handleHoverIn(e.currentTarget.id)}
                 onMouseLeave={(e) => handleHoverOut(e.currentTarget.id)}
@@ -33,8 +33,8 @@ export default function Gallery({ data }: { data?: galleryItem[] }) {
                 />
                 {hoverItem === 'image1' ? (
                     <>
-                        <h3 className="text-sm sm:text-xl z-10 font-medium text-white">Title</h3>
-                        <p className="text-sm sm:text-md z-10 text-white">Caption</p>
+                        <h3 className="text-sm sm:text-xl z-10 font-medium text-white">{data[0]?.title}</h3>
+                        <p className="text-sm sm:text-md z-10 text-white">{data[0]?.caption}</p>
                     </>
                 ) : (
                     <></>
@@ -42,7 +42,7 @@ export default function Gallery({ data }: { data?: galleryItem[] }) {
                 }
             </div>
             <div 
-                className="relative row-span-1 col-span-1 flex flex-col items-center justify-center"
+                className={`${data.length >= 3 ? "relative row-span-1 col-span-1 flex flex-col items-center justify-center" : data.length === 2 ? "relative row-span-1 col-span-1 flex flex-col items-center justify-center" : "hidden" }`}
                 id="image2"
                 onMouseEnter={(e) => handleHoverIn(e.currentTarget.id)}
                 onMouseLeave={(e) => handleHoverOut(e.currentTarget.id)}
@@ -56,8 +56,8 @@ export default function Gallery({ data }: { data?: galleryItem[] }) {
                 />
                 {hoverItem === 'image2' ? (
                     <>
-                        <h3 className="text-sm sm:text-xl z-10 font-medium text-white">Title</h3>
-                        <p className="text-sm sm:text-md z-10 text-white">Caption</p>
+                        <h3 className="text-sm sm:text-xl z-10 font-medium text-white">{data[1]?.title}</h3>
+                        <p className="text-sm sm:text-md z-10 text-white">{data[1]?.caption}</p>
                     </>
                 ) : (
                     <></>
@@ -65,7 +65,7 @@ export default function Gallery({ data }: { data?: galleryItem[] }) {
                 }
             </div>
             <div 
-                className="relative row-span-1 col-span-1 flex flex-col items-center justify-center"
+                className={`${data.length >= 3 ? "relative row-span-1 col-span-1 flex flex-col items-center justify-center" : "hidden"}`}
                 id="image3"
                 onMouseEnter={(e) => handleHoverIn(e.currentTarget.id)}
                 onMouseLeave={(e) => handleHoverOut(e.currentTarget.id)}
@@ -79,8 +79,8 @@ export default function Gallery({ data }: { data?: galleryItem[] }) {
                 />
                 {hoverItem === 'image3' ? (
                     <>
-                        <h3 className="text-sm sm:text-xl z-10 font-medium text-white">Title</h3>
-                        <p className="text-sm sm:text-md z-10 text-white">Caption</p>
+                        <h3 className="text-sm sm:text-xl z-10 font-medium text-white">{data[2]?.title}</h3>
+                        <p className="text-sm sm:text-md z-10 text-white">{data[2]?.caption}</p>
                     </>
                 ) : (
                     <></>
