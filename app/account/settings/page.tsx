c24add885904d4d617121cf386d925ddb0380b55
Eamon Gordon <ekeokigordon@icloud.com>
2024-01-02 22:20:29 -0800

    clean up login redirect exp

--- app/account/settings/page.tsx
+++ app/account/settings/page.tsx
@@ -9,7 +9,7 @@ import Footer from '@/components/footer';
 export default async function SettingsPage() {
     const session = await getSession();
     if (!session) {
-        redirect("/login");
+        redirect(`/login?redirect=/account/settings`);
     }
     return (
         <main>
