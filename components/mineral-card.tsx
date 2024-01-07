b48c0080dbb4ed31e3afbe4b93c6a753e557dbd5
Eamon Gordon <ekeokigordon@icloud.com>
2024-01-06 18:08:15 -0800

    add mineral card

---
+++ components/mineral-card.tsx
@@ -0,0 +1,20 @@
+import { Card, CardHeader, CardBody, CardFooter, Image, Button } from "@nextui-org/react";
+import Link from 'next/link';
+
+export default function MineralCard({ name }: { name: string }) {
+    return (
+        <Link href="/">
+            <Card isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-5">
+                <Image
+                    removeWrapper
+                    alt="Card example background"
+                    className="z-0 w-full h-full scale-125 -translate-y-6 object-cover"
+                    src="/Amazonite-106_horiz.jpeg"
+                />
+                <CardFooter className="absolute bg-white/30 bottom-0 z-10 justify-between">
+                    <p className="mx-auto text-lg font-semibold">{name}</p>
+                </CardFooter>
+            </Card >
+        </Link>
+    )
+}
