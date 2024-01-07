b8e0e4845ea23bb9ebd62781ceb6b02467674701
Eamon Gordon <ekeokigordon@icloud.com>
2024-01-06 16:24:53 -0800

    add photos and mineral query db

--- lib/actions.ts
+++ lib/actions.ts
@@ -34,7 +34,7 @@ export const editUser = async (
             "Missing BLOB_READ_WRITE_TOKEN token. Note: Vercel Blob is currently in beta – please fill out this form for access: https://tally.so/r/nPDMNd",
         };
       }
-      
+
       const file = formData.get(key) as File;
       const filename = `${nanoid()}.${file.type.split("/")[1]}`;
 
@@ -65,3 +65,73 @@ export const editUser = async (
     }
   }
 };
+
+export const fetchMinerals = async (
+  names: [],
+  minHardness?: number,
+  maxHardness?: number,
+  lusters?: [],
+  streaks?: [],
+  mineralClasses?: [],
+  chemistry?: [],
+  associates?: []
+) => {
+  let queryArray = [];
+  function pushArrayField(propertyArray: [], property: string) {
+    let filterArray: { property: { contains: string } }[] = [];
+    propertyArray.forEach((propertyItem) => {
+      let pushObj = { property: { contains: propertyItem } }
+      filterArray.push(pushObj);
+    })
+    queryArray.push({ OR: filterArray })
+  }
+  if (names && names.length > 0) {
+    let filterArray: { name: { contains: string } }[] = [];
+    names.forEach((name) => {
+      let pushObj = { name: { contains: name } }
+      filterArray.push(pushObj);
+    })
+    queryArray.push({ OR: filterArray })
+  }
+  if (minHardness) {
+    queryArray.push({ hardness_min: { equals: minHardness } })
+  }
+  if (maxHardness) {
+    queryArray.push({ hardness_max: { equals: maxHardness } })
+  }
+  if (lusters) {
+    pushArrayField(lusters, "luster");
+  }
+  if (streaks) {
+    pushArrayField(streaks, "streak");
+  }
+  if (mineralClasses) {
+    pushArrayField(mineralClasses, "mineral_class");
+  }
+  if (chemistry) {
+    pushArrayField(chemistry, "chemical_formula");
+  }
+  if (associates && associates.length > 0) {
+    let associatesArray: { name: { contains: string } }[] = [];
+    associates.forEach((associate) => {
+      let pushObj = { name: { contains: associate } }
+      associatesArray.push(pushObj);
+    })
+    queryArray.push({ associates: { some: { OR: [] } } })
+  }
+  const results = await prisma.mineral.findMany(
+    {
+      //@ts-expect-error;
+      where: { AND: queryArray },
+      select: {
+        name: true,
+        photos: {
+          select: {
+            image: true
+          }
+        }
+      }
+    }
+  );
+  return results;
+}; 
