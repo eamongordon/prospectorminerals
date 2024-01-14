2312fea42426040229d55612d0ea812bd47ded65
Eamon Gordon <ekeokigordon@icloud.com>
2024-01-13 20:38:00 -0800

    add photo db capabilities

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,12 +1,14 @@
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import Blurhash from "./blurhash";
+import PhotoAdd from "./photoItems";
 
 export default async function SettingsPage() {
     return (
         <main>
             <Header />
             <Blurhash />
+            <PhotoAdd />
             <Footer />
         </main>
     );
