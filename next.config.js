a7622fbd6ca635783cad3c455e80b878901deeea
Eamon Gordon <ekeokigordon@icloud.com>
2024-01-14 14:16:39 -0800

    add photo page and remotePatterns

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            { hostname: "ousfgajmtaam7m3j.public.blob.vercel-storage.com" },
+            { hostname: "public.blob.vercel-storage.com" },
+            { hostname: "res.cloudinary.com" },
+            { hostname: "avatar.vercel.sh" },
+            { hostname: "avatars.githubusercontent.com" },
+            { hostname: "www.google.com" },
+        ]
+    }
+}
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
