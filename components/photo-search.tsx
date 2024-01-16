097dd594c26f95e50702bd70df4d44a01a1a666c
Eamon Gordon <ekeokigordon@icloud.com>
2024-01-16 14:09:40 -0800

    photo page changes

--- components/photo-search.tsx
+++ components/photo-search.tsx
@@ -3,7 +3,8 @@
 import { useEffect, useRef, useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { Search as MagnifyingGlassIcon } from 'lucide-react'
-import { useDebounce } from 'use-debounce'
+import { useDebounce } from 'use-debounce';
+import { Input } from '@nextui-org/react';
 
 const Search = ({ search }: { search?: string }) => {
   const router = useRouter()
@@ -33,11 +34,14 @@ const Search = ({ search }: { search?: string }) => {
           aria-hidden='true'
         />
       </div>
-      <input
+      <Input
+        type="text"
         value={text}
-        placeholder='Search movies...'
+        placeholder="Search"
         onChange={e => setText(e.target.value)}
-        className='block w-full rounded-md border-0 py-1.5 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6'
+        endContent={
+          <MagnifyingGlassIcon />
+        }
       />
     </div>
   )
