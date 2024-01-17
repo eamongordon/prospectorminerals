65da4e1efd237c29eac403f57c496837efeb5569
Eamon Gordon <ekeokigordon@icloud.com>
2024-01-17 13:17:39 -0800

    center settings

--- app/account/settings/page.tsx
+++ app/account/settings/page.tsx
@@ -15,58 +15,60 @@ export default async function SettingsPage() {
     return (
         <main>
             <Header />
-            <div className="flex max-w-screen-xl flex-col space-y-12 p-8">
-                <div className="flex flex-col space-y-6">
-                    <h1 className="text-3xl font-semibold dark:text-white">
-                        Settings
-                    </h1>
-                    <Form
-                        title="Avatar"
-                        description="Your profile picture. Accepted formats: .png, .jpg, .jpeg"
-                        helpText="Max file size 50MB. Recommended size 400x400."
-                        inputAttrs={{
-                            name: "avatar",
-                            type: "file",
-                            defaultValue: session.user.image!
-                        }}
-                        handleSubmit={editUser}
-                    />
-                    <Form
-                        title="Name"
-                        description="Your name."
-                        helpText="Please use 32 characters maximum."
-                        inputAttrs={{
-                            name: "name",
-                            type: "text",
-                            defaultValue: session.user.name!,
-                            placeholder: "John Doe",
-                            maxLength: 32,
-                        }}
-                        handleSubmit={editUser}
-                    />
-                    <Form
-                        title="Email"
-                        description="Your email address used to login."
-                        helpText="Please enter a valid email."
-                        inputAttrs={{
-                            name: "email",
-                            type: "email",
-                            defaultValue: session.user.email!,
-                            placeholder: "email@example.com",
-                        }}
-                        handleSubmit={editUser}
-                    />
-                    <Form
-                        title="New Password"
-                        description="Create a new password"
-                        helpText="Please enter a valid password."
-                        inputAttrs={{
-                            name: "password",
-                            type: "password",
-                        }}
-                        handleSubmit={editUser}
-                    />
-                    <DeleteUserForm />
+            <div className="flex justify-center items-center">
+                <div className="flex w-full max-w-screen-xl flex-col space-y-12 p-8">
+                    <div className="flex flex-col space-y-6">
+                        <h1 className="text-3xl font-semibold dark:text-white">
+                            Settings
+                        </h1>
+                        <Form
+                            title="Avatar"
+                            description="Your profile picture. Accepted formats: .png, .jpg, .jpeg"
+                            helpText="Max file size 50MB. Recommended size 400x400."
+                            inputAttrs={{
+                                name: "avatar",
+                                type: "file",
+                                defaultValue: session.user.image!
+                            }}
+                            handleSubmit={editUser}
+                        />
+                        <Form
+                            title="Name"
+                            description="Your name."
+                            helpText="Please use 32 characters maximum."
+                            inputAttrs={{
+                                name: "name",
+                                type: "text",
+                                defaultValue: session.user.name!,
+                                placeholder: "John Doe",
+                                maxLength: 32,
+                            }}
+                            handleSubmit={editUser}
+                        />
+                        <Form
+                            title="Email"
+                            description="Your email address used to login."
+                            helpText="Please enter a valid email."
+                            inputAttrs={{
+                                name: "email",
+                                type: "email",
+                                defaultValue: session.user.email!,
+                                placeholder: "email@example.com",
+                            }}
+                            handleSubmit={editUser}
+                        />
+                        <Form
+                            title="New Password"
+                            description="Create a new password"
+                            helpText="Please enter a valid password."
+                            inputAttrs={{
+                                name: "password",
+                                type: "password",
+                            }}
+                            handleSubmit={editUser}
+                        />
+                        <DeleteUserForm />
+                    </div>
                 </div>
             </div>
             <Footer />
