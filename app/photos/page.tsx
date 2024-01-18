4c47f919084c09723bb5e200affa6244fdb5f15e
Eamon Gordon <ekeokigordon@icloud.com>
2024-01-17 21:39:24 -0800

    try implement sort

--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -8,8 +8,9 @@ const nanoid = customAlphabet(
 import { fetchPhotos } from '@/lib/actions'
 import Header from "@/components/header";
 import Footer from "@/components/footer";
-import Search from '@/components/photo-search'
-import InfiniteScrollPhotos from '@/components/infinite-scroll-photos'
+import Search from '@/components/photo-search';
+import SortDropdown from '@/components/photos/sort-dropdown';
+import InfiniteScrollPhotos from '@/components/infinite-scroll-photos';
 
 const Page = async ({
     searchParams
@@ -18,18 +19,23 @@ const Page = async ({
 }) => {
     const search =
         typeof searchParams.search === 'string' ? searchParams.search : undefined
-
+    const property =
+        typeof searchParams.property === 'string' ? searchParams.property : undefined
+    const order =
+        typeof searchParams.order === 'string' ? searchParams.order : undefined
     const photosQuery = await fetchPhotos({ filterObj: { name: search }, cursor: undefined, limit: 10 });
     return (
         <main>
             <Header />
             <div className="flex justify-center items-center">
-            <section className='flex-col justify-center items-center py-24 px-6 w-full max-w-screen-xl'>
+                <section className='flex-col justify-center items-center py-24 px-6 w-full max-w-screen-xl'>
                     <div className='mb-12 flex items-center justify-between gap-x-16'>
                         <h1 className='flex-1 text-3xl font-bold'>Photos</h1>
-
                         <div className='flex-1'>
                             <Search search={search} />
+                        </div>
+                        <div className='flex-1'>
+                            <SortDropdown sort={`${property},${order}`} />
                         </div>
                     </div>
 
@@ -38,9 +44,9 @@ const Page = async ({
                         role='list'
                         className='w-full grid grid-cols-2 gap-2 sm:gap-4 md:grid-cols-3 lg:grid-cols-5'
                     >
-                        <InfiniteScrollPhotos search={search} initialPhotos={photosQuery.results} initialCursor={photosQuery.next ? photosQuery.next : undefined}/>
+                        <InfiniteScrollPhotos search={search} initialPhotos={photosQuery.results} initialCursor={photosQuery.next ? photosQuery.next : undefined} {...(property && order ? { sort: { property: property, order: order } } : {})} />
                     </ul>
-            </section>
+                </section>
             </div>
             <Footer />
         </main >
