a1f26dc939a833a6e2a3ecb746830d139b20696e
Eamon Gordon <ekeokigordon@icloud.com>
2024-01-20 13:19:12 -0800

    add minerals main page

---
+++ app/minerals/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+
+export default function getMinerals() {
+    return (
+        <main>
+            <Header />
+            <Footer />
+        </main>
+    )
+}
