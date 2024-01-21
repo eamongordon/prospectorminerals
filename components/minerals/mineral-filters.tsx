b1902203ed5257e642720560e173a1f6960a7026
Eamon Gordon <ekeokigordon@icloud.com>
2024-01-20 17:34:59 -0800

    add suport minerals filter luster

--- components/minerals/mineral-filters.tsx
+++ components/minerals/mineral-filters.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import React from "react";
+import { useState, useEffect, useRef } from "react";
+import { useRouter, usePathname, useSearchParams } from 'next/navigation'
 import { Accordion, AccordionItem, Slider, CheckboxGroup, Checkbox, Input } from "@nextui-org/react";
-import { Search } from 'lucide-react';
+import { Search as MagnifyingGlassIcon } from 'lucide-react';
+import { useDebounce } from 'use-debounce';
 
-export default function MineralFilters() {
+export default function MineralFilters({ name, lusters }: { name?: string, lusters?:string[] | undefined }) {
     const defaultContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
     /*
         const mapAccordionItems = (label: string, value: string, optionsArray: [], stateName: string) => {
@@ -39,17 +41,58 @@ export default function MineralFilters() {
             )
         }
     */
-    const [isLusterInvalid, setIsLusterInvalid] = React.useState(false);
-    const [isStreakInvalid, setIsStreakInvalid] = React.useState(true);
+    const router = useRouter();
+    const pathname = usePathname();
+    const searchParams = useSearchParams();
+    const initialRender = useRef(true);
+    const [searchText, setSearchText] = useState(name);
+    const [lustersVal, setLustersVal] = useState<String[]>(lusters ? lusters : []);
+    const [isLusterInvalid, setIsLusterInvalid] = useState(false);
+    const [searchQuery] = useDebounce(searchText, 500);
+    
+    useEffect(() => {
+        if (initialRender.current) {
+            initialRender.current = false
+            return
+        }
+        const current = new URLSearchParams(Array.from(searchParams.entries())); // -> has to use this form
+        if (!searchQuery) {
+            current.delete("name");
+        } else {
+            current.set("name", searchQuery);
+        }
+        const search = current.toString();
+        const queryParam = search ? `?${search}` : "";
+        router.push(`${pathname}${queryParam}`);
+    }, [searchQuery]);
+
+    useEffect(() => {
+        if (initialRender.current) {
+            initialRender.current = false
+            return
+        }
+        const current = new URLSearchParams(Array.from(searchParams.entries())); // -> has to use this form
+        if (!searchQuery) {
+            current.delete("lusters");
+        } else {
+            current.set("lusters", lustersVal.join(','));
+        }
+        const search = current.toString();
+        const queryParam = search ? `?${search}` : "";
+        router.push(`${pathname}${queryParam}`);
+    }, [lustersVal]);
+
     return (
         <div className="w-80">
             <Input
                 type="text"
                 label="Search"
                 placeholder="Search"
+                value={searchText}
                 labelPlacement="outside"
+                onChange={e => setSearchText(e.target.value)}
                 endContent={
-                    <Search />
+                    <MagnifyingGlassIcon />
                 }
             />
             <Accordion>
@@ -62,7 +105,7 @@ export default function MineralFilters() {
                         minValue={0}
                         maxValue={10}
                         defaultValue={[0, 10]}
-                        className="max-w-md"
+                        className="max-w-[220px]"
                     />
                 </AccordionItem>
                 <AccordionItem key="2" aria-label="Lusters" title="Lusters">
@@ -75,6 +118,7 @@ export default function MineralFilters() {
                         defaultValue={["silky", "vitreous", "waxy", "submetallic", "metallic", "resinous", "pearly", "greasy", "dull", "adamantine"]}
                         onValueChange={(value) => {
                             setIsLusterInvalid(value.length < 1);
+                            setLustersVal(value);
                         }}
                     >
                         <Checkbox value="silky">Silky</Checkbox>
