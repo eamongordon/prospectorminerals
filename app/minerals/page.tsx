5f3346a4084e521863121410f43e1a6c12f66723
Eamon Gordon <ekeokigordon@icloud.com>
2024-01-20 16:37:59 -0800

    upgrade next 14.1 and page changes minerals

--- app/minerals/page.tsx
+++ app/minerals/page.tsx
@@ -11,6 +11,7 @@ import Footer from "@/components/footer";
 import Search from '@/components/photos/photo-search';
 import SortDropdown from '@/components/photos/sort-dropdown';
 import InfiniteScrollMinerals from '@/components/minerals/infinite-scroll-minerals';
+import MineralFilters from "@/components/minerals/mineral-filters";
 
 const Page = async ({
     searchParams
@@ -23,28 +24,22 @@ const Page = async ({
         typeof searchParams.property === 'string' ? searchParams.property : undefined
     const order =
         typeof searchParams.order === 'string' ? searchParams.order : undefined
-    const photosQuery = await fetchMinerals({ filterObj: { name: search }, cursor: undefined, limit: 10, ...(property && order ? { sortObj: {property: property, order: order} } : {})});
+    const photosQuery = await fetchMinerals({ filterObj: { name: search }, cursor: undefined, limit: 10, ...(property && order ? { sortObj: { property: property, order: order } } : {}) });
     return (
         <main>
             <Header />
             <div className="flex justify-center items-center">
                 <section className='flex-col justify-center items-center py-4 px-6 w-full max-w-screen-xl'>
-                    <div className='mb-4 sm:mb-12 flex-row my-5 sm:flex sm:gap-x-10 items-center justify-between'>
-                        <div className='py-2 sm:basis-2/3'>
-                            <Search search={search} />
-                        </div>
-                        <div className='py-2 sm:basis-1/3'>
-                            <SortDropdown {...(property && order ? { sort: `${property},${order}` } : {})} />
-                        </div>
+                    <div className='mb-4 sm:mb-12 flex-row my-5 sm:flex sm:gap-x-10 justify-between'>
+                        <MineralFilters />
+                        <ul
+                            key={nanoid()}
+                            role='list'
+                            className='w-full grid grid-cols-2 gap-3 sm:gap-4 md:grid-cols-3 lg:grid-cols-5'
+                        >
+                            <InfiniteScrollMinerals search={search} initialPhotos={photosQuery.results} initialCursor={photosQuery.next ? photosQuery.next : undefined} {...(property && order ? { sort: { property: property, order: order } } : {})} />
+                        </ul>
                     </div>
-
-                    <ul
-                        key={nanoid()}
-                        role='list'
-                        className='w-full grid grid-cols-2 gap-3 sm:gap-4 md:grid-cols-3 lg:grid-cols-5'
-                    >
-                        <InfiniteScrollMinerals search={search} initialPhotos={photosQuery.results} initialCursor={photosQuery.next ? photosQuery.next : undefined} {...(property && order ? { sort: { property: property, order: order } } : {})} />
-                    </ul>
                 </section>
             </div>
             <Footer />
