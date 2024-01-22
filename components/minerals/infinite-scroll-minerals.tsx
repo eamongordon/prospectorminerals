ff29dde0802676a134a5abe9d53a8c92cb2d9ad0
Eamon Gordon <ekeokigordon@icloud.com>
2024-01-22 08:51:56 -0800

    layout changes and render fix infinite scroll

--- components/minerals/infinite-scroll-minerals.tsx
+++ components/minerals/infinite-scroll-minerals.tsx
@@ -35,12 +35,14 @@ export default function InfiniteScrollPhotos({
   initialPhotos,
   initialCursor,
   sort,
+  key,
   clearFilters
 }: {
   filterObj: MineralsFilterObj | undefined
   initialPhotos: any[] | undefined
   initialCursor: number | undefined
-  sort?: PhotosSortObj | undefined
+  sort?: PhotosSortObj | undefined,
+  key?: string | undefined,
   clearFilters?: any | undefined
 }) {
 
@@ -69,41 +71,47 @@ export default function InfiniteScrollPhotos({
   }, [inView])
 
   return (
-    <>
-      {
-        photos?.map(mineral => (
-          <li key={mineral.id} className='relative'>
-            <div
-              className='relative flex flex-col items-center justify-center text-center group aspect-square w-full overflow-hidden rounded-lg'
-              id={mineral.id}
-            >
-              <MineralCard name={mineral.name} />
-            </div>
-          </li>
-        ))
-      }
-      {
-        photos?.length && photos.length > 0 ? (
-          <></>
-        ) : (
-          <div className='flex-col items-center justify-center col-span-1 sm:col-span-1 md:col-span-2 lg:col-span-3'>
+    <ul
+      key={key}
+      role='list'
+      className='w-full grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3'
+    >
+      <>
+        {
+          photos?.map(mineral => (
+            <li key={mineral.id} className='relative'>
+              <div
+                className='relative flex flex-col items-center justify-center text-center group w-full overflow-hidden rounded-lg'
+                id={mineral.id}
+              >
+                <MineralCard name={mineral.name} />
+              </div>
+            </li>
+          ))
+        }
+        {
+          photos?.length && photos.length > 0 ? (
+            <></>
+          ) : (
+            <div className='flex-col items-center justify-center col-span-1 sm:col-span-1 md:col-span-2 lg:col-span-3'>
               <p className='w-full text-center'>No Minerals Found. Try adjusting your filters</p>
               <div className='flex items-center justify-center'>
-              <Button className="flex" onClick={() => clearFilters ? clearFilters() : console.log("no clear func provided")}>
-                Clear Filters
-              </Button>
+                <Button className="flex" onClick={() => clearFilters ? clearFilters() : console.log("no clear func provided")}>
+                  Clear Filters
+                </Button>
               </div>
-          </div>
-        )
-      }
+            </div>
+          )
+        }
 
-      {/* loading spinner */}
-      <div
-        ref={ref}
-        className={`${!page ? "hidden" : ""} mt-16 mb-16 flex items-center justify-center col-span-1 sm:col-span-1 md:col-span-2 lg:col-span-3`}
-      >
-        <Spinner />
-      </div>
-    </>
+        {/* loading spinner */}
+        <div
+          ref={ref}
+          className={`${!page ? "hidden" : ""} mt-16 mb-16 flex items-center justify-center col-span-1 sm:col-span-1 md:col-span-2 lg:col-span-3`}
+        >
+          <Spinner />
+        </div>
+      </>
+    </ul>
   )
 }
