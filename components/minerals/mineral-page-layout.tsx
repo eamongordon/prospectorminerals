51c606624b67011746d18b576dcf463da8d647b0
Eamon Gordon <ekeokigordon@icloud.com>
2024-01-21 22:37:41 -0800

    latest changes ul page minerals

--- components/minerals/mineral-page-layout.tsx
+++ components/minerals/mineral-page-layout.tsx
@@ -266,13 +266,7 @@ export default function MineralPageLayout({
                         )
                     }
                 </div>
-                <ul
-                    key={nanoid()}
-                    role='list'
-                    className='w-full grid grid-cols-1 gap-3 sm:gap-4 md:grid-cols-2 lg:grid-cols-3'
-                >
                     {renderChildren()}
-                </ul>
             </div>
         </>
     );
