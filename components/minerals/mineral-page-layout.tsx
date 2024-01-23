2a6f3babfe6fb1e8e7228593f2a4ffae92fce15a
Eamon Gordon <ekeokigordon@icloud.com>
2024-01-22 16:41:39 -0800

    test take out infinite scroll minerals

--- components/minerals/mineral-page-layout.tsx
+++ components/minerals/mineral-page-layout.tsx
@@ -267,7 +267,6 @@ export default function MineralPageLayout({
                         )
                     }
                 </div>
-                    {renderChildren()}
             </div>
         </>
     );
