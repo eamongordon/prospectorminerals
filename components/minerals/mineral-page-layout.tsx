99e43dee06f9b096f014c490f659e1ee18b53345
Eamon Gordon <ekeokigordon@icloud.com>
2024-01-23 13:40:05 -0800

    switch to only setting defaultValue

--- components/minerals/mineral-page-layout.tsx
+++ components/minerals/mineral-page-layout.tsx
@@ -182,7 +182,7 @@ export default function MineralPageLayout({
                         type="text"
                         label="Search"
                         placeholder="Search"
-                        value={searchText || ""}
+                        defaultValue={searchText || ""}
                         labelPlacement="outside"
                         onChange={e => setSearchText(e.target.value)}
                         endContent={
@@ -198,8 +198,7 @@ export default function MineralPageLayout({
                                 color="foreground"
                                 minValue={0}
                                 maxValue={10}
-                                defaultValue={[0, 10]}
-                                value={hardnessVal || [0, 10]}
+                                defaultValue={hardnessVal || [0, 10]}
                                 className="w-full pr-3"
                                 onChangeEnd={value => setHardnessVal(value as number[])}
                             />
@@ -211,8 +210,7 @@ export default function MineralPageLayout({
                                 description="Select lusters to filter by"
                                 isInvalid={isLusterInvalid}
                                 label="Select lusters"
-                                value={lustersVal || ["silky", "vitreous", "waxy", "submetallic", "metallic", "resinous", "pearly", "greasy", "dull", "adamantine"]}
-                                defaultValue={["silky", "vitreous", "waxy", "submetallic", "metallic", "resinous", "pearly", "greasy", "dull", "adamantine"]}
+                                defaultValue={lustersVal || ["silky", "vitreous", "waxy", "submetallic", "metallic", "resinous", "pearly", "greasy", "dull", "adamantine"]}
                                 onValueChange={(value) => {
                                     setIsLusterInvalid(value.length < 1);
                                     setLustersVal(value);
