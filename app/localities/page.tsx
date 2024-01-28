624e550268ec90ae7a4a84ea051aab9eb356c2a0
Eamon Gordon <ekeokigordon@icloud.com>
2024-01-28 15:12:34 -0800

    restore localities page

--- app/localities/page.tsx
+++ app/localities/page.tsx
@@ -14,56 +14,6 @@ const markers = [
     }
 ];
 
-export default function Home() {
-    return null;
-    /*
-    const DynamicMap = useMemo(() => dynamic(
-        () => import('@/components/localities/map'),
-        {
-            loading: () => (<Skeleton className="h-80 w-80" />),
-            ssr: false
-        }
-    ), []);
-    return (
-        <>
-            <Header />
-            <main>
-                <DynamicMap markers={markers} />
-            </main>
-            <Footer />
-        </>
-    )
-    */
-}
-/*
-'use client';
-import { useMemo } from 'react';
-import dynamic from 'next/dynamic';
-
-export default function MyPage() {
-    const Map = useMemo(() => dynamic(
-      () => import('@/components/localities/map'),
-      { 
-        loading: () => <p>A map is loading</p>,
-        ssr: false
-      }
-    ), [])
-  
-    return <div>
-      <Map center={[51.505, -0.09]} zoom={13}/>
-    </div>
-  }
-  */
-/*
-  import { customAlphabet } from "nanoid";
-
-const nanoid = customAlphabet(
-    "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
-    7,
-); // 7-character random string
-
-
-
 const Page = async ({
     searchParams
 }: {
@@ -75,24 +25,21 @@ const Page = async ({
         typeof searchParams.property === 'string' ? searchParams.property : undefined
     const order =
         typeof searchParams.order === 'string' ? searchParams.order : undefined
+        const Map = useMemo(() => dynamic(
+            () => import('@/components/localities/map'),
+            { 
+              loading: () => <Skeleton className='h-[200px] w-[200px]'/>,
+              ssr: false
+            }
+          ), [])
     //const photosQuery = await fetchPhotos({ filterObj: { name: search }, cursor: undefined, limit: 10, ...(property && order ? { sortObj: { property: property, order: order } } : {}) });
     return (
         <main>
             <Header />
-            <div className="flex justify-center items-center">
-                <section className='flex-col justify-center items-center py-4 px-6 w-full max-w-screen-xl'>
-                    
-                    <PhotosLayout
-                        infiniteScrollElem={<InfiniteScrollPhotos search={search} initialPhotos={photosQuery.results} initialCursor={photosQuery.next ? photosQuery.next : undefined} {...(property && order ? { sort: { property: property, order: order } } : {})} key={nanoid()} />}
-                        search={search}
-                        sortDropdownElem={<SortDropdown {...(property && order ? { sort: `${property},${order}` } : {})} />}
-                    />
-                </section>
-            </div>
+            <Map markers={markers}/>
             <Footer />
         </main >
     )
 }
 
 export default Page;
-*/
