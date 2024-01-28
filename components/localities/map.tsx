04f2d4b7d3247bd6fdb934f2910054256f21dd8e
Eamon Gordon <ekeokigordon@icloud.com>
2024-01-28 14:18:00 -0800

    new commit back to norm

--- components/localities/map.tsx
+++ components/localities/map.tsx
@@ -1,13 +1,13 @@
 'use client'
 
-//import './popup-style.css'
-//import 'leaflet/dist/leaflet.css'
-//import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import './popup-style.css'
+import 'leaflet/dist/leaflet.css'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import { useState } from 'react'
 import Link from 'next/link'
 import { Link as UILink, Accordion, AccordionItem, Button } from '@nextui-org/react'
-//import "leaflet-defaulticon-compatibility"
-//import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
+import "leaflet-defaulticon-compatibility"
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
 
 
 export default function Map({ markers }: { markers?: any }) {
@@ -41,6 +41,60 @@ export default function Map({ markers }: { markers?: any }) {
         )
     }
 
-    return (<></>
+    return (
+        <div>
+            <SearchLocation />
+            <GetMyLocation />
+            <Button onClick={() => setStateMarkers([{title: 'New Marker', coords: [10, 10]}])}>More Markers</Button>
+            <div className="flex w-full flex-col sm:flex-row">
+                <div className="w-full sm:w-80">
+                    <Accordion>
+                        <AccordionItem key="hardness" aria-label="Hardness" title="Hardness">
+                            <p>Some text</p>
+                        </AccordionItem>
+
+                    </Accordion>
+                </div>
+                <div className="flex-col items-center w-full">
+                <MapContainer
+                    className='w-full max-h-[400px] aspect-[5/3]'
+                    //@ts-expect-error
+                    center={coord} zoom={2} scrollWheelZoom={false}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+
+                    {
+                        stateMarkers.map((marker: any) => {
+                            return (
+                                <Marker key={marker.title} position={marker.coords} >
+                                    <Popup>
+                                        A pretty CSS3 popup. <br /> Easily customizable.
+                                    </Popup>
+                                </Marker>
+                            )
+                        })
+                    }
+                    {/*@ts-expect-error*/}
+                    <Marker position={coord} eventHandlers={{
+                        click: (e) => {
+                            console.log('marker clicked')
+                        },
+                        mousedown: (e) => {
+
+                        }
+                    }}>
+                        <Popup className='request-popup'>
+                            <p className='text-lg'>Large Tezt</p>
+                            <UILink href='/'>This is a link</UILink>
+                            <button className='w-full h-full bg-teal-200'>Large Tezt</button>
+                            A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                </MapContainer>
+                </div>
+            </div>
+        </div >
     )
 }
