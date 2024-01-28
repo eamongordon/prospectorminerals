c63abaa04f3871ce9bbd2fbcdb8d4c0667e83c90
Eamon Gordon <ekeokigordon@icloud.com>
2024-01-28 13:46:25 -0800

    test build time

--- app/localities/page.tsx
+++ app/localities/page.tsx
@@ -51,3 +51,45 @@ export default function MyPage() {
     </div>
   }
   */
+/*
+  import { customAlphabet } from "nanoid";
+
+const nanoid = customAlphabet(
+    "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+    7,
+); // 7-character random string
+
+
+
+const Page = async ({
+    searchParams
+}: {
+    searchParams: { [key: string]: string | string[] | undefined }
+}) => {
+    const search =
+        typeof searchParams.search === 'string' ? searchParams.search : undefined
+    const property =
+        typeof searchParams.property === 'string' ? searchParams.property : undefined
+    const order =
+        typeof searchParams.order === 'string' ? searchParams.order : undefined
+    //const photosQuery = await fetchPhotos({ filterObj: { name: search }, cursor: undefined, limit: 10, ...(property && order ? { sortObj: { property: property, order: order } } : {}) });
+    return (
+        <main>
+            <Header />
+            <div className="flex justify-center items-center">
+                <section className='flex-col justify-center items-center py-4 px-6 w-full max-w-screen-xl'>
+                    
+                    <PhotosLayout
+                        infiniteScrollElem={<InfiniteScrollPhotos search={search} initialPhotos={photosQuery.results} initialCursor={photosQuery.next ? photosQuery.next : undefined} {...(property && order ? { sort: { property: property, order: order } } : {})} key={nanoid()} />}
+                        search={search}
+                        sortDropdownElem={<SortDropdown {...(property && order ? { sort: `${property},${order}` } : {})} />}
+                    />
+                </section>
+            </div>
+            <Footer />
+        </main >
+    )
+}
+
+export default Page;
+*/
