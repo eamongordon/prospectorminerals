8014b4dc5f0bd822c5683b1cc5774c5bf6e421bb
Eamon Gordon <ekeokigordon@icloud.com>
2024-02-03 16:30:10 -0800

    install next-mdx-remote & add component

---
+++ components/mdx.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { Post } from "@prisma/client";
+import { MDXRemote, MDXRemoteProps } from "next-mdx-remote";
+import { replaceLinks } from "@/lib/remark-plugins";
+import BlurImage from "@/components/blur-image";
+
+export default function MDX({ source }: { source: MDXRemoteProps }) {
+  const components = {
+    a: replaceLinks,
+    BlurImage
+  };
+
+  return (
+    <article
+      className={`prose-md prose prose-stone m-auto w-11/12 dark:prose-invert sm:prose-lg sm:w-3/4 ${styles.root}`}
+      suppressHydrationWarning={true}
+    >
+      {/* @ts-ignore */}
+      <MDXRemote {...source} components={components} />
+    </article>
+  );
+}
