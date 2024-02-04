8a50016475070bd68e3a6a23cf0acae97e033937
Eamon Gordon <ekeokigordon@icloud.com>
2024-02-03 18:50:01 -0800

    add post card component and page

--- app/manage/posts/page.tsx
+++ app/manage/posts/page.tsx
@@ -1,44 +1,36 @@
 import { getSession } from "@/lib/auth";
 import prisma from "@/lib/prisma";
 import { notFound, redirect } from "next/navigation";
-//import Posts from "@/components/posts";
-//import CreatePostButton from "@/components/create-post-button";
+import Posts from "@/components/posts";
+import CreatePostButton from "@/components/create-post-button";
 
 export default async function SitePosts({
   params,
 }: {
   params: { id: string };
 }) {
-    return(<><p>Hey</p></>)
-    /*
+
   const session = await getSession();
   if (!session) {
     redirect("/login");
   }
-  const data = await prisma.site.findUnique({
-    where: {
-      id: decodeURIComponent(params.id),
-    },
-  });
 
-  if (!data || data.userId !== session.user.id) {
-    notFound();
-  }
-
-  const url = `${data.subdomain}.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`;
+  const url = `${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`;
 
   return (
     <>
       <div className="flex flex-col items-center justify-between space-y-4 sm:flex-row sm:space-y-0">
         <div className="flex flex-col items-center space-y-2 sm:flex-row sm:space-x-4 sm:space-y-0">
           <h1 className="w-60 truncate font-cal text-xl font-bold dark:text-white sm:w-auto sm:text-3xl">
-            All Posts for {data.name}
+            All Posts
           </h1>
           <a
-            href={
+            href={"/"
+              /*
               process.env.NEXT_PUBLIC_VERCEL_ENV
                 ? `https://${url}`
                 : `http://${data.subdomain}.localhost:3000`
+                */
             }
             target="_blank"
             rel="noreferrer"
@@ -52,5 +44,4 @@ export default async function SitePosts({
       <Posts siteId={decodeURIComponent(params.id)} />
     </>
   );
-  */
 }
