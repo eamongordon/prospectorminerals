7e39236bdafff402e85289556f1229f4379d8d82
Eamon Gordon <ekeokigordon@icloud.com>
2024-02-20 19:21:32 -0800

    Update Leaflet and Login Vault modals

--- components/registration/login-vaul-modal.tsx
+++ components/registration/login-vaul-modal.tsx
@@ -12,9 +12,9 @@ const DrawerScrollableWithInputs: FC<DrawerScrollableWithInputsProps> = () => {
       <Drawer.Trigger asChild>
         <button className="bg-slate-800 text-white py-1 px-3 rounded-md m-2">Drawer</button>
       </Drawer.Trigger>
-      <Drawer.Portal>
+      <Drawer.Portal >
         <Drawer.Overlay className="fixed inset-0 bg-black/40" />
-        <Drawer.Content className="flex flex-col fixed bottom-0 left-0 right-0 max-h-[85vh] rounded-t-[10px] z-50">
+        <Drawer.Content className="flex flex-col fixed bottom-0 left-0 right-0 max-h-[85vh] rounded-t-[10px] z-50 overflow-auto">
           <LoginModal/>
         </Drawer.Content>
       </Drawer.Portal>
