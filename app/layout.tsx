319809747aff0376a43a98f7c653c2248934d6a1
Eamon Gordon <ekeokigordon@icloud.com>
2024-02-22 20:52:46 -0800

    Add headers and useragent to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Work_Sans } from 'next/font/google'
 import './globals.css';
 import { Providers } from './providers';
 import { Analytics } from "@vercel/analytics/react";
+import { headers } from 'next/headers'
+import { user } from '@nextui-org/react';
 
 const inter = Work_Sans({ subsets: ['latin'] })
 
@@ -16,9 +18,11 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  const headersList = headers();
+  const useragent = headersList.get('User-Agent');
   return (
     <html lang="en">
-      {navigator.userAgent.includes('iPhone') ?
+      {useragent?.includes('iPhone') ?
         (<head><meta name="viewport" content="width=device-width, initial-scale=1 maximum-scale=1" /></head>) : (null)
       }
       <body className={`${inter.className} bg-gradient-to-b from-[rgb(214,219,220)] to-white text-black dark:bg-gradient-to-b dark:from-black dark:to-black dark:text-white`}>
