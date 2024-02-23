f9a147610af1b6b091998d6b86daea4b6c590629
Eamon Gordon <ekeokigordon@icloud.com>
2024-02-23 10:24:09 -0800

    change overflow behavior modal

--- components/next-ui-modal.tsx
+++ components/next-ui-modal.tsx
@@ -2,22 +2,24 @@
 
 import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure } from "@nextui-org/react";
 import LoginForm from '@/components/registration/login-form';
+import { Drawer } from "vaul";
 
 export default function ModalNext({ isMobile }: { isMobile?: boolean }) {
     const { isOpen, onOpen, onOpenChange } = useDisclosure();
     return (
         <>
-            <Button onPress={onOpen} color="default" variant="flat" className={`${isMobile ? "px-4 mx-auto": "hidden sm:flex"}`}>Log In</Button>
+            <Button onPress={onOpen} color="default" variant="flat" className={`${isMobile ? "px-4 mx-auto" : "hidden sm:flex"}`}>Log In</Button>
             <Modal
                 backdrop="opaque"
                 isOpen={isOpen}
                 onOpenChange={onOpenChange}
                 classNames={{
-                    backdrop: "bg-gradient-to-t from-zinc-900 to-zinc-900/10 backdrop-opacity-20"
+                    backdrop: "bg-gradient-to-t from-zinc-900 to-zinc-900/10 backdrop-opacity-20",
+                    base: ['sm:hidden']
                 }}
                 radius="none"
                 hideCloseButton={true}
-                {...(isMobile ? {scrollBehavior:"outside"} : {})}
+                {...(isMobile ? { scrollBehavior: "outside" } : {})}
             >
                 <ModalContent className="overflow-y-scroll bg-inherit shadow-none">
                     {(onClose) => (
