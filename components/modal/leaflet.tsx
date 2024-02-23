6040baf30d6f63b989fc165b8c4fa3845c872838
Eamon Gordon <ekeokigordon@icloud.com>
2024-02-23 10:24:09 -0800

    change overflow behavior modal

--- components/modal/leaflet.tsx
+++ components/modal/leaflet.tsx
@@ -53,9 +53,7 @@ export default function Leaflet({
           <div className="-mr-1 h-1 w-6 rounded-full bg-gray-300 transition-all group-active:rotate-12" />
           <div className="h-1 w-6 rounded-full bg-gray-300 transition-all group-active:-rotate-12" />
         </div>
-        <div className="overflow-auto">
-          {children}
-        </div>
+        {children}
       </motion.div>
       <motion.div
         key="leaflet-backdrop"
