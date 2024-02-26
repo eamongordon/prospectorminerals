243527c75f4500f4c5b3d228f1a6517104a771b0
Eamon Gordon <ekeokigordon@icloud.com>
2024-02-26 10:37:24 -0800

    Add type imports and update filterObj in minerals page

--- app/minerals/page.tsx
+++ app/minerals/page.tsx
@@ -6,6 +6,7 @@ const nanoid = customAlphabet(
 ); // 7-character random string
 
 import { fetchMinerals } from '@/lib/actions'
+import { LustersList, MineralClassesList, CrystalSystemsList } from '@/types/types'
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import InfiniteScrollMinerals from '@/components/minerals/infinite-scroll-minerals';
@@ -20,11 +21,11 @@ const Page = async ({
     const name =
         typeof searchParams.name === 'string' ? searchParams.name : undefined
     const lusters =
-        typeof searchParams.lusters === 'string' ? searchParams.lusters : undefined
+        typeof searchParams.lusters === 'string' ? searchParams.lusters.split(',') as LustersList[] : undefined
     const mineralClasses =
-        typeof searchParams.mineralClasses === 'string' ? searchParams.mineralClasses : undefined
+        typeof searchParams.mineralClasses === 'string' ? searchParams.mineralClasses?.split(',') as  MineralClassesList[]: undefined
     const crystalSystems =
-        typeof searchParams.crystalSystems === 'string' ? searchParams.crystalSystems : undefined
+        typeof searchParams.crystalSystems === 'string' ? searchParams.crystalSystems?.split(',') as CrystalSystemsList[] : undefined
     const chemistry =
         typeof searchParams.chemistry === 'string' ? searchParams.chemistry : undefined
     const minHardness =
@@ -35,7 +36,7 @@ const Page = async ({
         typeof searchParams.property === 'string' ? searchParams.property : undefined
     const order =
         typeof searchParams.order === 'string' ? searchParams.order : undefined
-    const filterObj = { name: name, lusters: lusters?.split(','), mineralClasses: mineralClasses?.split(','), crystalSystems: crystalSystems?.split(','), chemistry: chemistry?.split(','), minHardness: Number(minHardness), maxHardness: Number(maxHardness) }
+    const filterObj = { name: name, lusters: lusters, mineralClasses: mineralClasses, crystalSystems: crystalSystems, chemistry: chemistry?.split(','), minHardness: Number(minHardness), maxHardness: Number(maxHardness) }
     const photosQuery = await fetchMinerals({ filterObj: filterObj, cursor: undefined, limit: 10, ...(property && order ? { sortObj: { property: property, order: order } } : {}) });
     console.log(typeof searchParams.mineralClasses);
     return (
