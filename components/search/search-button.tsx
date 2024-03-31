b4c14f31681878bfa99b1d0e0e85dac41fc8e53a
Eamon Gordon <ekeokigordon@icloud.com>
2024-03-30 20:47:57 -0700

    Add search functionality to header and prevent scrolling when modal is open

---
+++ components/search/search-button.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useModal } from "@/components/modal/provider";
+import { ReactNode, Dispatch, SetStateAction } from "react";
+import { Button } from "@nextui-org/react";
+import { Search } from 'lucide-react';
+
+export default function SearchModalButton({
+    isMobile,
+    children,
+    closeMenuCallback
+}: {
+    isMobile?: boolean
+    children: ReactNode;
+    closeMenuCallback?: Dispatch<SetStateAction<boolean>>;
+}) {
+    const modal = useModal();
+    return (
+        <Button
+            onClick={() => { if (closeMenuCallback) { closeMenuCallback(false); } modal?.show(children); }}
+            isIconOnly color="default" variant="faded" aria-label="Search">
+            <Search />
+        </Button>
+    );
+}
+
+/*
+    <Button 
+      onClick={() => {if (closeMenuCallback) {closeMenuCallback(false);} modal?.show(children);}}
+      color="default" variant="flat" className={`${isMobile ? "px-4": "hidden sm:flex"}`}
+    >
+      Log In
+    </Button>
+*/
