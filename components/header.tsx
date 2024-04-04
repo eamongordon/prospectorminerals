c7ab7e182d7153810d02c36e5de6af8bdab9f129
Eamon Gordon <ekeokigordon@icloud.com>
2024-04-04 08:31:42 -0700

    Refactor header component, include searchModalProvider for logged in and out.

--- components/header.tsx
+++ components/header.tsx
@@ -2,28 +2,29 @@ import HeaderContent from './header-content'
 import { getSession } from "@/lib/auth";
 import { LoginModalProvider } from './modal/registration/provider';
 import { SearchModalProvider } from './modal/search/provider';
-//
+
 export default async function Header() {
   const session = await getSession();
   const { name, image, email } = session?.user || {};
-  return session ? (
-    <HeaderContent
-      loggedIn={true}
-      userData={
-        {
-          name: name,
-          image: image,
-          email: email
-        }
-      }
-    />
-  ) : (
-    <LoginModalProvider>
-      <SearchModalProvider>
-      <HeaderContent
-        loggedIn={false}
-      />
-      </SearchModalProvider>
-    </LoginModalProvider>
-  )
+  return (
+    <SearchModalProvider>
+      {session ? (
+        <HeaderContent
+          loggedIn={true}
+          userData={
+            {
+              name: name,
+              image: image,
+              email: email
+            }
+          }
+        />) : (
+        <LoginModalProvider>
+          <HeaderContent
+            loggedIn={false}
+          />
+        </LoginModalProvider>
+      )}
+    </SearchModalProvider>
+  );
 }
