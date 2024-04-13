1980e86b1bfdea6e017e37169180017ec498d6bf
Eamon Gordon <ekeokigordon@icloud.com>
2024-04-13 11:18:35 -0400

    Add search results section to page

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -12,6 +12,14 @@ const Page = async ({
             <Header />
             <div className="flex justify-center items-center">
                 <section className='flex-col justify-center items-center py-4 px-6 w-full max-w-screen-xl'>
+                    <h1 className="text-3xl font-bold text-center">Search Results</h1>
+                    <div className="'w-full flex-col sm:flex-row">
+                        <div className="w-full sm:w-80">
+                            <div className="w-full">   
+                            
+                            </div>
+                        </div>
+                    </div>
                 </section>
             </div>
             <Footer />
