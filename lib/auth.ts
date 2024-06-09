de117e80c7c27fbea336bcdbc9b910dfabdece4f
Eamon Gordon <ekeokigordon@icloud.com>
2024-06-08 19:38:23 -0700

    refactor: Remove unused import in auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,4 +1,4 @@
-import { AuthOptions, getServerSession, type NextAuthOptions } from "next-auth";
+import { getServerSession, type NextAuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
