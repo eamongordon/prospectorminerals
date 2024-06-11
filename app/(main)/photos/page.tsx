f81b00454fa19de78cdf83f34a80e19e6edc8b4b
Eamon Gordon <ekeokigordon@icloud.com>
2024-06-11 16:45:39 -0700

    refactor: Remove commented out code in photos page

--- app/(main)/photos/page.tsx
+++ app/(main)/photos/page.tsx
@@ -26,24 +26,6 @@ const Page = async ({
         <main>
             <div className="flex justify-center items-center">
                 <section className='flex-col justify-center items-center py-4 px-6 w-full max-w-screen-xl'>
-                    {/*
-                    <div className='mb-4 sm:mb-12 flex-row my-5 sm:flex sm:gap-x-10 items-center justify-between'>
-                        <div className='py-2 sm:basis-2/3'>
-                            <Search search={search} />
-                        </div>
-                        <div className='py-2 sm:basis-1/3'>
-                            <SortDropdown {...(property && order ? { sort: `${property},${order}` } : {})} />
-                        </div>
-                    </div>
-
-                    <ul
-                        key={nanoid()}
-                        role='list'
-                        className='w-full grid grid-cols-2 gap-3 sm:gap-4 md:grid-cols-3 lg:grid-cols-5'
-                    >
-                        <InfiniteScrollPhotos search={search} initialPhotos={photosQuery.results} initialCursor={photosQuery.next ? photosQuery.next : undefined} {...(property && order ? { sort: { property: property, order: order } } : {})} />
-                    </ul>
-    */}
                     <PhotosLayout
                         infiniteScrollElem={<InfiniteScrollPhotos search={search} initialPhotos={photosQuery.results} initialCursor={photosQuery.next ? photosQuery.next : undefined} {...(property && order ? { sort: { property: property, order: order } } : {})} key={nanoid()} />}
                         search={search}
