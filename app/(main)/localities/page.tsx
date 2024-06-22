c4b09e204321965bd69e6dbc3d3f0eb531207e88
Eamon Gordon <ekeokigordon@icloud.com>
2024-06-21 17:23:31 -0700

    refactor: Add Clear Filters button component for localities page

--- app/(main)/localities/page.tsx
+++ app/(main)/localities/page.tsx
@@ -1,10 +1,11 @@
 import dynamic from 'next/dynamic';
 import { useMemo } from 'react';
-import { Skeleton } from '@nextui-org/react';
+import { Button } from '@nextui-org/react';
 import LocalitiesPageLayout from "@/components/localities/localities-page-layout"
 import MineralSelect from "@/components/localities/mineral-search-inner"
 import { fetchLocalities, fetchMinerals } from '@/lib/actions';
 import type { mineralListItem } from '@/types/types';
+import ClearFilters from '@/components/localities/clear-filters';
 
 const markers = [
     {
@@ -39,13 +40,18 @@ const Page = async ({
     ), [])
     */
     const filterObj = { name: name };
-    const localities = await fetchLocalities({ filterObj: {...filterObj, minerals: minerals ? minerals.map((obj : mineralListItem) => obj.name) : undefined}, cursor: undefined, limit: 100, ...(property && order ? { sortObj: { property: property, order: order } } : {}) });
+    const localities = await fetchLocalities({ filterObj: { ...filterObj, minerals: minerals ? minerals.map((obj: mineralListItem) => obj.name) : undefined }, cursor: undefined, limit: 100, ...(property && order ? { sortObj: { property: property, order: order } } : {}) });
+    //TODO: make Button re-render with new key
+    const serializedKey = JSON.stringify({ filterObj, property, order });
     return (
         <main>
             <LocalitiesPageLayout
                 markers={markers}
-                filterObj={{...filterObj, minerals: minerals}}
+                filterObj={{ ...filterObj, minerals: minerals }}
                 localities={localities.results}
+                clearButton={
+                    <ClearFilters key={serializedKey}/>
+                }
             />
         </main >
     )
---
+++ components/localities/locality-card.tsx
@@ -0,0 +1,22 @@
+import { Card, CardHeader, CardBody, CardFooter, Image as UIImage, Button } from "@nextui-org/react";
+import Link from 'next/link';
+//import BlurImage from './blur-ui-image';
+
+export default function LocalityCard({ name, id, image, blurDataURL }: { name: string, id: string, image?: string, blurDataURL?: string }) {
+    return (
+        <Link href={`/minerals/${id}`}>
+            <Card isFooterBlurred className="w-full">
+                <UIImage
+                    removeWrapper
+                    alt="Card example background"
+                    className="z-0 w-full h-full scale-125 -translate-y-6 object-cover"
+                    src="/Amazonite-106_horiz.jpeg"
+                    fallbackSrc="data:image/png;base64,/9j/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAoADIDASIAAhEBAxEB/8QAGwAAAwADAQEAAAAAAAAAAAAAAAYHBAUIAwL/xAAmEAACAgICAQMEAwAAAAAAAAABAgADBREEBiESEyIHMUGRFHGh/8QAGQEAAwEBAQAAAAAAAAAAAAAAAQIDBQAE/8QAGxEAAgIDAQAAAAAAAAAAAAAAAAIBEQMhMQT/2gAMAwEAAhEDEQA/AOWEqdz4EzeNjLbSNKYzYHDDkFfEoGI6snxJSGiLZlXpOsf1e24A+gzM5HULlTYrP6l3wnXqFCgqIwW9Zotq0EEm00UxvD8OR+fg7qCdqZqLaWrOiJ0t2vqCIjsqf5In2jF/xbX+OtTlaykrQnwn0V8mEcUqHUeXWpTZEp/AyFK1Loic+YfINQw8xv4vYCqAeuUWTL9OFmnRZ+JmlVwAY24rJ+6o87nPXC7Bu0fOPvXuxIAu3k8lSP5cbpOyoZiheTxm+O9iQj6g4YhrGCyvUdgpenRcfaIvduZRfW+iJ511JsdggT49g7ePzCMdnt+439mEtZKhHSwr9p7DluPzCEYSok96cg6HfqM3XA7DZTr5GEIJDGjeU9wsVde4f3MDJdmfkKQXJhCChrkXzkmJJ3CEIaBZ/9k="
+                />
+                <CardFooter className="absolute bg-white/30 bottom-0 z-10 justify-between">
+                    <p className="mx-auto text-white text-lg font-semibold">{name}</p>
+                </CardFooter>
+            </Card >
+        </Link>
+    )
+}
