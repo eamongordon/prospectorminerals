606b9304513b2dd9a2363ff7b39afea3249248cb
Eamon Gordon <ekeokigordon@icloud.com>
2024-06-21 17:39:31 -0700

    refactor: Rename clearFilters function to ClearFilters component in clear-filters.tsx

--- components/localities/clear-filters.tsx
+++ components/localities/clear-filters.tsx
@@ -3,7 +3,7 @@
 import { Button } from "@nextui-org/react";
 import { useState } from 'react';
 
-export default function clearFilters({ clearFilters }: { clearFilters?: () => void }) {
+export default function ClearFilters({ clearFilters }: { clearFilters?: () => void }) {
     const [noResultsLoading, setNoResultsLoading] = useState(false);
     return (
         <Button className="flex" onClick={() => {setNoResultsLoading(true); clearFilters!(); }} isLoading={noResultsLoading}>
