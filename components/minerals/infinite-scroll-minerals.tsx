d508b4c1fb6d790c664fc95897cf729f5de40889
Eamon Gordon <ekeokigordon@icloud.com>
2024-06-23 08:12:34 -0700

    refactor: Update MineralCard component to use slug instead of id

--- components/minerals/infinite-scroll-minerals.tsx
+++ components/minerals/infinite-scroll-minerals.tsx
@@ -71,7 +71,7 @@ export default function InfiniteScrollPhotos({
         {
           photos?.map(mineral => (
             <li key={mineral.id} className='relative flex flex-col items-center justify-center text-center group w-full overflow-hidden rounded-xl'>
-                <MineralCard name={mineral.name} id={mineral.id}/>
+                <MineralCard name={mineral.name} slug={mineral.id}/>
             </li>
           ))
         }
