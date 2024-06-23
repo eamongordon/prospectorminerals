be8f66e50b10d76fac79cb0d3e96ad49efe3a25d
Eamon Gordon <ekeokigordon@icloud.com>
2024-06-23 12:16:30 -0700

    refactor: Add MineralTags component for displaying mineral tags

--- app/(main)/minerals/[slug]/page.tsx
+++ app/(main)/minerals/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { fetchMinerals } from "@/lib/actions";
 import Gallery from "@/components/minerals/mineral-gallery";
 import { notFound } from "next/navigation";
 import PropertyTable from "@/components/minerals/property-table";
+import MineralTags from "@/components/minerals/mineral-tags";
 
 const galleryData = [
     {
@@ -30,6 +31,15 @@ const htmltest = `<p class=""font_8"">Aurichalcite is commonly associated with:<
   <li><p class=""font_8"">Rosasite</p></li>
 </ul>`
 
+const mineralTagsTest = [{
+    name: "Test1",
+    image: 'https://i.pravatar.cc/300?u=a042581f4e29026709d',
+    id: 'test1'
+}, {
+    name: "Test2",
+    image: 'https://i.pravatar.cc/300?u=a042581f4e29026709d',
+    id: 'test2'
+}]
 export default async function Page({ params }: { params: { slug: string } }) {
     const mineralResult = await fetchMinerals({ filterObj: { id: params.slug }, cursor: undefined, limit: 1, fieldset: 'full' });
     let mineral;
@@ -47,8 +57,12 @@ export default async function Page({ params }: { params: { slug: string } }) {
                     <h1 className='font-semibold text-4xl sm:text-6xl py-4'>{mineral.name}</h1>
                     <Gallery data={galleryData} />
                     <div className="flex w-full flex-col sm:flex-row my-10">
-                        <div className="flex w-full">
+                        <div className="w-full">
                             <p>Test</p>
+                            <div className="space-y-2">
+                                <h2 className="font-semibold text-3xl sm:text-4xl mb-4">Associates</h2>
+                                <MineralTags tags={mineralTagsTest} />
+                            </div>
                         </div>
                         <div className="w-full my-4 sm:my-0 sm:w-[40%] sm:min-w-80">
                             <PropertyTable mineral={mineral} />
