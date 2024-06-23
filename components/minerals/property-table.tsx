0886c342c382eb14c03ac203a107c97c5706104b
Eamon Gordon <ekeokigordon@icloud.com>
2024-06-23 13:56:13 -0700

    refactor: Update PropertyTable component to dynamically display mineral properties

--- components/minerals/property-table.tsx
+++ components/minerals/property-table.tsx
@@ -1,4 +1,5 @@
 'use client'
+
 import {
     Table,
     TableHeader,
@@ -9,31 +10,36 @@ import {
 } from "@nextui-org/table";
 import { Mineral } from "@prisma/client";
 
-export default function PropertyTable({mineral} : {mineral: Mineral}) {
-    return (
+export default function PropertyTable({ mineral }: { mineral: Mineral }) {
+    let tableData = [];
+    if (mineral.chemical_formula) {
+        tableData.push({ property: "Chemical Formula", value: mineral.chemical_formula });
+    }
+    if (mineral.hardness_min && mineral.hardness_max) {
+        tableData.push({ property: "Hardness", value: mineral.hardness_min === mineral.hardness_max ? mineral.hardness_min.toString() : mineral.hardness_min.toString() + '-' + mineral.hardness_max.toString() });
+    }
+    if (mineral.crystal_system) {
+        tableData.push({ property: "Crystal System", value: mineral.crystal_system });
+    }
+    if (mineral.mineral_class) {
+        tableData.push({ property: "Mineral Class", value: mineral.mineral_class });
+    }
+    return tableData.length ? (
         <Table shadow="none">
-        <TableHeader>
-            <TableColumn className="text-md">Properties</TableColumn>
-            <TableColumn><></></TableColumn>
-        </TableHeader>
-        <TableBody>
-            <TableRow>
-                <TableCell className="font-semibold">Chemistry</TableCell>
-                <TableCell>{mineral.chemical_formula}</TableCell>
-            </TableRow>
-            <TableRow>
-                <TableCell className="font-semibold">Hardness</TableCell>
-                <TableCell>{mineral.hardness_min?.toString() + '-' + mineral.hardness_max?.toString()}</TableCell>
-            </TableRow>
-            <TableRow>
-                <TableCell className="font-semibold">Crystal System</TableCell>
-                <TableCell>{mineral.crystal_system}</TableCell>
-            </TableRow>
-            <TableRow>
-                <TableCell className="font-semibold">Mineral Class</TableCell>
-                <TableCell>{mineral.mineral_class} </TableCell>
-            </TableRow>
-        </TableBody>
-    </Table>
-    )
+            <TableHeader>
+                <TableColumn className="text-md">Properties</TableColumn>
+                <TableColumn><></></TableColumn>
+            </TableHeader>
+            <TableBody>
+                {tableData.map((row, index) => {
+                    return (
+                        <TableRow key={index}>
+                            <TableCell>{row.property}</TableCell>
+                            <TableCell>{row.value}</TableCell>
+                        </TableRow>
+                    )
+                })}
+            </TableBody>
+        </Table >
+    ) : (<></>)
 }
