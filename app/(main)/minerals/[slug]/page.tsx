cc16987e5772e0e8a6505774f6f8734a643d4322
Eamon Gordon <ekeokigordon@icloud.com>
2024-06-24 09:03:50 -0700

    refactor: Update mineral page to display additional mineral information

--- app/(main)/minerals/[slug]/page.tsx
+++ app/(main)/minerals/[slug]/page.tsx
@@ -19,18 +19,6 @@ const galleryData = [
     }
 ];
 
-const htmltest = `<p class=""font_8"">Aurichalcite is commonly associated with:</p>
-<p class=""font_8""><br></p>
-<ul class=""font_8"">
-  <li><p class=""font_8"">Hemimorphite</p></li>
-  <li><p class=""font_8"">Calcite (as Inclusions)</p></li>
-  <li><p class=""font_8"">Azurite</p></li>
-  <li><p class=""font_8"">Malachite</p></li>
-  <li><p class=""font_8"">Cuprite</p></li>
-  <li><p class=""font_8"">Smithsonite</p></li>
-  <li><p class=""font_8"">Rosasite</p></li>
-</ul>`
-
 const mineralTagsTest = [{
     name: "Test1",
     image: 'https://i.pravatar.cc/300?u=a042581f4e29026709d',
@@ -39,7 +27,8 @@ const mineralTagsTest = [{
     name: "Test2",
     image: 'https://i.pravatar.cc/300?u=a042581f4e29026709d',
     id: 'test2'
-}]
+}];
+
 export default async function Page({ params }: { params: { slug: string } }) {
     const mineralResult = await fetchMinerals({ filterObj: { id: params.slug }, cursor: undefined, limit: 1, fieldset: 'full' });
     let mineral;
@@ -48,17 +37,31 @@ export default async function Page({ params }: { params: { slug: string } }) {
     } else {
         mineral = mineralResult.results[0];
     }
+    let descriptionFields = [];
+    if (mineral.uses) {
+        descriptionFields.push({ property: "Uses", value: mineral.uses });
+    }
+    if (mineral.localities_description) {
+        descriptionFields.push({ property: "Notable Localities", value: mineral.localities_description });
+    }
     return (
         <main>
             <div className="flex justify-center items-center">
                 <section className='flex-col justify-center items-center py-4 px-6 w-full max-w-screen-xl'>
-                    <div className='mb-4 sm:mb-12 flex-row my-5 sm:flex sm:gap-x-10 justify-between'></div>
-                    <div>My Post: {params.slug}</div>
+                    <div className='flex-row sm:flex sm:gap-x-10 justify-between'></div>
                     <h1 className='font-semibold text-4xl sm:text-6xl py-4'>{mineral.name}</h1>
                     <Gallery data={galleryData} />
                     <div className="flex w-full flex-col sm:flex-row my-10">
-                        <div className="w-full">
-                            <p>Test</p>
+                        <div className="w-full sm:pr-12">
+                            {mineral.description ? (<p>{mineral.description}</p>) : (<></>)}
+                            {descriptionFields.map((obj) => {
+                                return (
+                                    <div className="space-y-2 my-6">
+                                        <h2 className="font-semibold text-3xl sm:text-4xl mb-4">{obj.property}</h2>
+                                        <p>{obj.value}</p>
+                                    </div>
+                                )
+                            })}
                             <div className="space-y-2">
                                 <h2 className="font-semibold text-3xl sm:text-4xl mb-4">Associates</h2>
                                 <MineralTags tags={mineralTagsTest} />
