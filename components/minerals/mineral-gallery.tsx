05a8790e727e2bfb75f0edadda5cd1b037515ee5
Eamon Gordon <ekeokigordon@icloud.com>
2024-06-25 18:32:53 -0700

    refactor: change mobile aspect ratio and center text

--- components/minerals/mineral-gallery.tsx
+++ components/minerals/mineral-gallery.tsx
@@ -9,7 +9,7 @@ type galleryItem = {
 
 export default function Gallery({ data }: { data: galleryItem[] }) {
     return (
-        <div className={`${data.length >= 3 ? "grid grid-rows-2 grid-cols-3 gap-2 sm:gap-4 max-w-screen-xl aspect-[8/3] max-h-[500px] mx-auto" : data.length === 2 ? "grid grid-rows-1 grid-cols-2 gap-2 sm:gap-4 max-w-screen-xl aspect-[8/3] max-h-[500px] px-8 mx-auto" : "grid grid-rows-1 grid-cols-1 gap-2 sm:gap-4 max-w-screen-xl aspect-[8/3] max-h-[500px] px-8 mx-auto"}`}>
+        <div className={`${data.length >= 3 ? "grid grid-rows-2 grid-cols-3 gap-2 sm:gap-4 max-w-screen-xl aspect-[2/1] sm:aspect-[8/3] max-h-[500px] mx-auto" : data.length === 2 ? "grid grid-rows-1 grid-cols-2 gap-2 sm:gap-4 max-w-screen-xl aspect-[8/3] max-h-[500px] px-8 mx-auto" : "grid grid-rows-1 grid-cols-1 gap-2 sm:gap-4 max-w-screen-xl aspect-[8/3] max-h-[500px] px-8 mx-auto"}`}>
             <div
                 className={`group ${data.length >= 3 ? "relative rounded-md row-span-2 col-span-2 flex flex-col items-center justify-center" : "relative rounded-md row-span-1 col-span-1 flex flex-col items-center justify-center"}`}
                 id="image1"
@@ -22,8 +22,8 @@ export default function Gallery({ data }: { data: galleryItem[] }) {
                     fill={true}
                 />
                 <>
-                    <h3 className="group-hover:block hidden text-sm sm:text-xl z-10 font-medium text-white">{data[0]?.title}</h3>
-                    <p className="group-hover:block hidden text-sm sm:text-md z-10 text-white">{data[0]?.caption}</p>
+                    <h3 className="text-center group-hover:block hidden text-sm sm:text-xl z-10 font-medium text-white">{data[0]?.title}</h3>
+                    <p className="text-center group-hover:block hidden text-sm sm:text-md z-10 text-white">{data[0]?.caption}</p>
                 </>
             </div>
             <div
@@ -39,8 +39,8 @@ export default function Gallery({ data }: { data: galleryItem[] }) {
                 />
 
                 <>
-                    <h3 className="group-hover:block hidden text-sm sm:text-xl z-10 font-medium text-white">{data[1]?.title}</h3>
-                    <p className="group-hover:block hidden text-sm sm:text-md z-10 text-white">{data[1]?.caption}</p>
+                    <h3 className="text-center group-hover:block hidden text-sm sm:text-xl z-10 font-medium text-white">{data[1]?.title}</h3>
+                    <p className="text-center group-hover:block hidden text-sm sm:text-md z-10 text-white">{data[1]?.caption}</p>
                 </>
             </div>
             <div
@@ -55,8 +55,8 @@ export default function Gallery({ data }: { data: galleryItem[] }) {
                     objectFit='cover'
                 />
                 <>
-                    <h3 className="group-hover:block hidden text-sm sm:text-xl z-10 font-medium text-white">{data[2]?.title}</h3>
-                    <p className="group-hover:block hidden text-sm sm:text-md z-10 text-white">{data[2]?.caption}</p>
+                    <h3 className="text-center group-hover:block hidden text-sm sm:text-xl z-10 font-medium text-white">{data[2]?.title}</h3>
+                    <p className="text-center group-hover:block hidden text-sm sm:text-md z-10 text-white">{data[2]?.caption}</p>
                 </>
             </div>
         </div>
