f38f7ea9c83d7622ac102f2ef2ceef2fbf383335
Eamon Gordon <ekeokigordon@icloud.com>
2024-06-26 18:32:48 -0700

    chore: Add lazy loading for LocalityMap component

--- app/(main)/localities/[slug]/page.tsx
+++ app/(main)/localities/[slug]/page.tsx
@@ -1,6 +1,8 @@
+import { Suspense } from 'react'
 import { fetchLocalities } from '@/lib/actions';
 import { notFound } from "next/navigation";
 import LocalityMap from '@/components/localities/locality-map';
+import { Skeleton } from '@nextui-org/react';
 
 export default async function Page({ params }: { params: { slug: string } }) {
     const localityResult = await fetchLocalities({ filterObj: { id: params.slug }, cursor: undefined, limit: 1, fieldset: 'full' });
@@ -11,6 +13,8 @@ export default async function Page({ params }: { params: { slug: string } }) {
         locality = localityResult.results[0];
     }
     return (
-        <LocalityMap localities={[locality]} center={[locality.latitude as unknown as number, locality.longitude as unknown as number]} zoom={6} />
+        <Suspense fallback={<Skeleton className='h-[400px] w-full'/>}>
+            <LocalityMap localities={[locality]} center={[locality.latitude as unknown as number, locality.longitude as unknown as number]} zoom={6} />
+        </Suspense>
     );
 };
