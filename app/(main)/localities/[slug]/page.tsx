0a785c25a806b8daabdd79d6fa3d9abf979ae940
Eamon Gordon <ekeokigordon@icloud.com>
2024-06-28 19:42:47 -0700

    refactor: update locality page

--- app/(main)/localities/[slug]/page.tsx
+++ app/(main)/localities/[slug]/page.tsx
@@ -1,8 +1,36 @@
 import LocalityMap from '@/components/localities/locality-map';
+import PropertyTable from '@/components/property-table';
+import Gallery from '@/components/minerals/mineral-gallery';
+import MineralTags from '@/components/minerals/mineral-tags';
 import { fetchLocalities } from '@/lib/actions';
 import { Skeleton } from '@nextui-org/react';
 import { notFound } from "next/navigation";
 import { Suspense } from 'react';
+
+const mineralTagsTest = [{
+    name: "Test1",
+    image: 'https://i.pravatar.cc/300?u=a042581f4e29026709d',
+    id: 'test1'
+}, {
+    name: "Test2",
+    image: 'https://i.pravatar.cc/300?u=a042581f4e29026709d',
+    id: 'test2'
+}];
+
+const galleryData = [
+    {
+        title: "Amazonite",
+        caption: "Smoky Hawk Mine, Routte Co., Colorado"
+    },
+    {
+        title: "Cavansite",
+        caption: "Pune, Maharashtra Province, India"
+    },
+    {
+        title: "Krohnkite",
+        caption: "Chuquicamata Mine, El Loa Province, Chile"
+    }
+];
 
 export default async function Page({ params }: { params: { slug: string } }) {
     const localityResult = await fetchLocalities({ filterObj: { id: params.slug }, cursor: undefined, limit: 1, fieldset: 'full' });
@@ -12,9 +40,39 @@ export default async function Page({ params }: { params: { slug: string } }) {
     } else {
         locality = localityResult.results[0];
     }
+    let tableData = [];
+    if (locality.longitude) {
+        tableData.push({ property: "Longitude", value: locality.longitude.toString() });
+    }
+    if (locality.latitude) {
+        tableData.push({ property: "Latitude", value: locality.latitude.toString() });
+    }
+    if (locality.type) {
+        tableData.push({ property: "Type", value: locality.type === 'Single' ? "Mine" : "Mining District" });
+    }
     return (
-        <Suspense fallback={<Skeleton className='h-[400px] w-full'/>}>
-            <LocalityMap localities={[locality]} center={[locality.longitude as unknown as number, locality.latitude as unknown as number]} zoom={6} />
-        </Suspense>
+        <main>
+            <div className='px-6 max-w-screen-xl mx-auto py-1 sm:py-3'>
+                <h1 className='font-semibold text-4xl sm:text-6xl py-4'>{locality.name}</h1>
+            </div>
+            <Suspense fallback={<Skeleton className='h-[400px] w-full' />}>
+                <LocalityMap localities={[locality]} center={[locality.longitude as unknown as number, locality.latitude as unknown as number]} zoom={6} />
+            </Suspense>
+            <div className='px-6 max-w-screen-xl mx-auto space-y-6 my-6 sm:space-y-10 sm:my-10'>
+                <p>{locality.description}</p>
+                <div className='flex flex-col md:flex-row gap-4 md:gap-8'>
+                    <div className='w-full md:w-2/3'>
+                        <Gallery data={galleryData} />
+                    </div>
+                    <div className='w-full md:w-1/3'>
+                        <PropertyTable data={tableData} />
+                    </div>
+                </div>
+                <div className="space-y-2">
+                    <h2 className="font-semibold text-3xl sm:text-4xl mb-4">Minerals</h2>
+                    <MineralTags tags={mineralTagsTest} />
+                </div>
+            </div>
+        </main>
     );
 };
