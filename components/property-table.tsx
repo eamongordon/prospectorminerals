47f26503ec9bccfcd27ad884c03da61180dc722f
Eamon Gordon <ekeokigordon@icloud.com>
2024-06-28 19:28:32 -0700

    refactor: Replace deprecated PropertyTable component
    The deprecated PropertyTable component has been replaced with a new implementation in order to improve code maintainability and adhere to best practices.

---
+++ components/property-table.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import {
+    Table,
+    TableHeader,
+    TableBody,
+    TableColumn,
+    TableRow,
+    TableCell
+} from "@nextui-org/table";
+
+type TableData = {
+    property: string;
+    value: string;
+}
+
+export default function PropertyTable({ data }: { data: TableData[] }) {
+    return data.length ? (
+        <Table shadow="none">
+            <TableHeader>
+                <TableColumn className="text-md">Properties</TableColumn>
+                <TableColumn><></></TableColumn>
+            </TableHeader>
+            <TableBody>
+                {data.map((row, index) => {
+                    return (
+                        <TableRow key={index}>
+                            <TableCell>{row.property}</TableCell>
+                            <TableCell>{row.value}</TableCell>
+                        </TableRow>
+                    )
+                })}
+            </TableBody>
+        </Table >
+    ) : (<></>)
+}
