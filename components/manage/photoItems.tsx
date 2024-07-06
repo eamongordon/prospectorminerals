33de5033a4d2933c69abdaecedad0a1c621dbc8e
Eamon Gordon <ekeokigordon@icloud.com>
2024-07-06 13:56:20 -0700

    feat: Add functionality to bulk create localities in the database

--- components/manage/photoItems.tsx
+++ components/manage/photoItems.tsx
@@ -1,7 +1,7 @@
 
 "use client"
 
-import { createPhotoBulk, createMineralBulk, createRelationsBulk } from "@/lib/actions";
+import { createPhotoBulk, createMineralBulk, createRelationsBulk, createLocalityBulk } from "@/lib/actions";
 import { useState } from "react";
 import { Button, Input } from "@nextui-org/react";
 import { toast } from "sonner";
@@ -43,6 +43,17 @@ export default function PhotoAdd() {
             }
         })
     }
+    function actionLocalitiesFunc() {
+        setLoading(true);
+        createLocalityBulk(textInput).then((res: any) => {
+            setLoading(false);
+            if (res.error) {
+                toast.error(res.error);
+            } else {
+                toast.success("Added minerals to DB");
+            }
+        })
+    }
     return (
         <>
             <div className="flex max-w-screen-xl flex-col space-y-12 p-8">
@@ -62,6 +73,16 @@ export default function PhotoAdd() {
                     </h1>
                     <Input placeholder="Enter JSON" type="text" onChange={(event) => setTextInput(event.target.value)} />
                     <Button onClick={() => actionMineralsFunc()} isLoading={loading}>Confirm</Button>
+                    <p className="break-words">{result}</p>
+                </div>
+            </div>
+            <div className="flex max-w-screen-xl flex-col space-y-12 p-8">
+                <div className="flex flex-col space-y-6">
+                    <h1 className="text-3xl font-semibold dark:text-white">
+                        Add Localities to DB
+                    </h1>
+                    <Input placeholder="Enter JSON" type="text" onChange={(event) => setTextInput(event.target.value)} />
+                    <Button onClick={() => actionLocalitiesFunc()} isLoading={loading}>Confirm</Button>
                     <p className="break-words">{result}</p>
                 </div>
             </div>
