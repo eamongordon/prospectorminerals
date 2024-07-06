66da4a252c2f51476108d31278ca2537f44a7936
Eamon Gordon <ekeokigordon@icloud.com>
2024-07-06 14:56:00 -0700

    refactor: work images for locality into code

--- components/localities/locality-map.tsx
+++ components/localities/locality-map.tsx
@@ -9,12 +9,12 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import L from 'leaflet';
 import Link from 'next/link';
 import { Card, CardFooter, Skeleton, Image as UIImage } from "@nextui-org/react";
-import { Locality } from '@prisma/client';
 import { Suspense } from 'react';
+import { LocalityDisplayFieldset } from '@/types/prisma';
 
 const inter = Work_Sans({ subsets: ['latin'] })
 
-export default function LocalityMap({ localities, center, zoom }: { localities: Locality[], center: [number, number], zoom: number}) {
+export default function LocalityMap({ localities, center, zoom }: { localities: LocalityDisplayFieldset[], center: [number, number], zoom: number }) {
     const singleLocalityKnownIcon = L.icon({
         iconUrl: '/localities/PM-Single-Locality-Pin_Light.png', // Replace with your icon's path
         iconSize: [35, 35], // Adjust based on your icon's dimensions
@@ -40,36 +40,36 @@ export default function LocalityMap({ localities, center, zoom }: { localities: 
     });
     return (
         <Suspense fallback={<Skeleton className='h-[400px] w-full'></Skeleton>}>
-        <MapContainer
-            className='w-full max-h-[400px] aspect-[5/3] z-0 py-0'
-            center={center} zoom={zoom} scrollWheelZoom={false}>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            {localities.map((locality: Locality) => {
-                return (<Marker key={locality.id} position={[Number(locality.latitude), Number(locality.longitude)]} icon={locality.type === 'Single' ? locality.coordinates_known ? singleLocalityKnownIcon : singleLocalityEstimatedIcon : locality.coordinates_known ? groupLocalityKnownIcon : groupLocalityEstimatedIcon} >
-                    <Popup className={`${inter.className} w-[200px]`} offset={[0, -21]}>
-                        <div key={locality.id} className='flex items-center justify-center text-center w-full overflow-hidden rounded-xl'>
-                            <Link href={`/localities/${locality.id}`}>
-                                <Card isFooterBlurred className="w-full">
-                                    <UIImage
-                                        removeWrapper
-                                        alt={locality.name}
-                                        className="z-0 w-full h-full scale-125 -translate-y-6 object-cover"
-                                        src="/Amazonite-106_horiz.jpeg"
-                                        fallbackSrc="data:image/png;base64,/9j/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAoADIDASIAAhEBAxEB/8QAGwAAAwADAQEAAAAAAAAAAAAAAAYHBAUIAwL/xAAmEAACAgICAQMEAwAAAAAAAAABAgADBREEBiESEyIHMUGRFHGh/8QAGQEAAwEBAQAAAAAAAAAAAAAAAQIDBQAE/8QAGxEAAgIDAQAAAAAAAAAAAAAAAAIBEQMhMQT/2gAMAwEAAhEDEQA/AOWEqdz4EzeNjLbSNKYzYHDDkFfEoGI6snxJSGiLZlXpOsf1e24A+gzM5HULlTYrP6l3wnXqFCgqIwW9Zotq0EEm00UxvD8OR+fg7qCdqZqLaWrOiJ0t2vqCIjsqf5In2jF/xbX+OtTlaykrQnwn0V8mEcUqHUeXWpTZEp/AyFK1Loic+YfINQw8xv4vYCqAeuUWTL9OFmnRZ+JmlVwAY24rJ+6o87nPXC7Bu0fOPvXuxIAu3k8lSP5cbpOyoZiheTxm+O9iQj6g4YhrGCyvUdgpenRcfaIvduZRfW+iJ511JsdggT49g7ePzCMdnt+439mEtZKhHSwr9p7DluPzCEYSok96cg6HfqM3XA7DZTr5GEIJDGjeU9wsVde4f3MDJdmfkKQXJhCChrkXzkmJJ3CEIaBZ/9k="
-                                    />
-                                    <CardFooter className="absolute bg-white/30 bottom-0 z-10 flex justify-center items-center">
-                                        <p className="mx-auto text-white text-lg font-semibold locality-card-text">{locality.name}</p>
-                                    </CardFooter>
-                                </Card >
-                            </Link>
-                        </div>
-                    </Popup>
-                </Marker>)
-            })}
-        </MapContainer>
+            <MapContainer
+                className='w-full max-h-[400px] aspect-[5/3] z-0 py-0'
+                center={center} zoom={zoom} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {localities.map((locality) => {
+                    return (<Marker key={locality.id} position={[Number(locality.latitude), Number(locality.longitude)]} icon={locality.type === 'Single' ? locality.coordinates_known ? singleLocalityKnownIcon : singleLocalityEstimatedIcon : locality.coordinates_known ? groupLocalityKnownIcon : groupLocalityEstimatedIcon} >
+                        <Popup className={`${inter.className} w-[200px]`} offset={[0, -21]}>
+                            <div key={locality.id} className='flex items-center justify-center text-center w-full overflow-hidden rounded-xl'>
+                                <Link href={`/localities/${locality.id}`}>
+                                    <Card isFooterBlurred className="w-full">
+                                        <UIImage
+                                            removeWrapper
+                                            alt={locality.name}
+                                            className="z-0 w-full h-full scale-125 -translate-y-6 object-cover aspect-[5/4]"
+                                            src={locality.photos.length > 0 && locality.photos[0].image ? locality.photos[0].image : "/Amazonite-106_horiz.jpeg"}
+                                            fallbackSrc="data:image/png;base64,/9j/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAoADIDASIAAhEBAxEB/8QAGwAAAwADAQEAAAAAAAAAAAAAAAYHBAUIAwL/xAAmEAACAgICAQMEAwAAAAAAAAABAgADBREEBiESEyIHMUGRFHGh/8QAGQEAAwEBAQAAAAAAAAAAAAAAAQIDBQAE/8QAGxEAAgIDAQAAAAAAAAAAAAAAAAIBEQMhMQT/2gAMAwEAAhEDEQA/AOWEqdz4EzeNjLbSNKYzYHDDkFfEoGI6snxJSGiLZlXpOsf1e24A+gzM5HULlTYrP6l3wnXqFCgqIwW9Zotq0EEm00UxvD8OR+fg7qCdqZqLaWrOiJ0t2vqCIjsqf5In2jF/xbX+OtTlaykrQnwn0V8mEcUqHUeXWpTZEp/AyFK1Loic+YfINQw8xv4vYCqAeuUWTL9OFmnRZ+JmlVwAY24rJ+6o87nPXC7Bu0fOPvXuxIAu3k8lSP5cbpOyoZiheTxm+O9iQj6g4YhrGCyvUdgpenRcfaIvduZRfW+iJ511JsdggT49g7ePzCMdnt+439mEtZKhHSwr9p7DluPzCEYSok96cg6HfqM3XA7DZTr5GEIJDGjeU9wsVde4f3MDJdmfkKQXJhCChrkXzkmJJ3CEIaBZ/9k="
+                                        />
+                                        <CardFooter className="absolute bg-white/30 bottom-0 z-10 flex justify-center items-center">
+                                            <p className="mx-auto text-white text-lg font-semibold locality-card-text">{locality.name}</p>
+                                        </CardFooter>
+                                    </Card >
+                                </Link>
+                            </div>
+                        </Popup>
+                    </Marker>)
+                })}
+            </MapContainer>
         </Suspense>
     )
 }
