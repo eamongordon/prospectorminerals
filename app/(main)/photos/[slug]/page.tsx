df457d47ff4c337a78c271da2079c54a52dae785
Eamon Gordon <ekeokigordon@icloud.com>
2024-07-06 15:47:13 -0700

    refactor: Update photo page to display locality and specimen dimensions
    This commit updates the `page.tsx` file in the `photos/[slug]` directory. It modifies the code to display the locality name and specimen dimensions (length, width, and height) of a photo on the photo page. This enhancement improves the information provided to users and enhances the overall user experience.
    
    Co-authored-by: [Author Name]

--- app/(main)/photos/[slug]/page.tsx
+++ app/(main)/photos/[slug]/page.tsx
@@ -1,24 +1,8 @@
 import MineralTags from '@/components/minerals/mineral-tags';
 import PhotoSlugImage from '@/components/photos/photo-slug-image';
 import { fetchPhotos } from '@/lib/actions';
-import prisma from "@/lib/prisma";
 import { MineralListItem } from '@/types/types';
 import { notFound } from 'next/navigation';
-
-const galleryData = [
-    {
-        title: "Amazonite",
-        caption: "Smoky Hawk Mine, Routte Co., Colorado"
-    },
-    {
-        title: "Cavansite",
-        caption: "Pune, Maharashtra Province, India"
-    },
-    {
-        title: "Krohnkite",
-        caption: "Chuquicamata Mine, El Loa Province, Chile"
-    }
-];
 
 export default async function Page({ params }: { params: { slug: string } }) {
     const photoResult = await fetchPhotos({ filterObj: { id: params.slug }, cursor: undefined, limit: 1, fieldset: 'full' });
@@ -40,14 +24,16 @@ export default async function Page({ params }: { params: { slug: string } }) {
                 </div>
                 <div className='flex-col items-center justify-start px-8 text-left space-y-3'>
                     <p className='text-4xl font-semibold'>{photo.name ? photo.name : ""}</p>
-                    <p className='text-lg font-semibold'>{"Smoky Hawk Claim, Lake George Co., Colorado"}</p>
-                    <p className='text-md'>{"6.0 x 4.3 x 2.5 cm"}</p>
+                    <p className='text-lg font-semibold'>{photo.locality ? photo.locality.name : photo.locality_fallback}</p>
+                    {photo.specimen_height && photo.specimen_length && photo.specimen_width ?
+                        <p className='text-md'>{`${photo.specimen_length} x ${photo.specimen_width} x ${photo.specimen_height} cm`}</p>
+                        : null}
                     <p className='text-md'>{photo.description ? photo.description : ""}</p>
-                    {photo.minerals.length ? 
-                    <>
-                    <h2 className='text-2xl font-semibold'>Minerals In this Photo</h2>
-                    <MineralTags tags={photo.minerals.map((obj) => { return { name: obj.mineral.name, image: obj.mineral.photos[0].photo.image, id: obj.mineral.id } as MineralListItem })} /> 
-                    </>: null}
+                    {photo.minerals.length ?
+                        <>
+                            <h2 className='text-2xl font-semibold'>Minerals In this Photo</h2>
+                            <MineralTags tags={photo.minerals.map((obj) => { return { name: obj.mineral.name, image: obj.mineral.photos[0].photo.image, id: obj.mineral.id } as MineralListItem })} />
+                        </> : null}
                 </div>
             </div>
         </main>
