f83d2bd4f404d564264c857b7561334ae9a2aac5
Eamon Gordon <ekeokigordon@icloud.com>
2024-07-07 11:33:15 -0700

    feat: add layout for articles

--- components/articles/infinite-scroll-articles.tsx
+++ components/articles/infinite-scroll-articles.tsx
@@ -7,7 +7,8 @@ import { customAlphabet } from "nanoid";
 import { Spinner, Button } from "@nextui-org/react";
 import type { ArticlesFilterObj, MineralsFilterObj, PhotosSortObj } from '@/types/types';
 import type { Post } from '@prisma/client';
-
+import BlurImage from '../blur-image';
+import Link from 'next/link';
 const nanoid = customAlphabet(
   "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
   7,
@@ -55,13 +56,40 @@ export default function InfiniteScrollArticles({
   return (
     <ul
       role='list'
-      className='w-full grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3'
+      className='w-full'
     >
       <>
         {
           articles?.length && articles.length > 0 ? (
             articles?.map(article => (
-              <p>{article.title}</p>
+              <li key={article.id}>
+                <Link href={`/articles/${article.slug}`} className='flex w-full flex-col sm:flex-row gap-8'>
+                  {article.image ? (
+                    <div className="w-full sm:w-[45%] h-52 sm:h-80"> {/* This div ensures BlurImage can be styled to fill its parent */}
+                      <BlurImage
+                        alt='Article Image'
+                        src={article.image}
+                        width={500}
+                        height={350}
+                        className="w-full h-full object-cover rounded-xl"
+                        blurDataURL={article.imageBlurhash || undefined}
+                      // Add a className here if you need to apply specific styles
+                      />
+                    </div>
+                  ) : null}
+                  <div className={`${article.image ? "w-[55%]" : ""} flex flex-col justify-center gap-4`}>
+                    <p className="text-sm opacity-70">
+                      {new Date(article.createdAt).toLocaleDateString('en-US', {
+                        month: 'short', // "Feb"
+                        day: '2-digit', // "23"
+                        year: 'numeric', // "2024"
+                      })}
+                    </p>
+                    <h3 className="text-4xl font-bold">{article.title}</h3>
+                    <p className="text-lg">{article.description}</p>
+                  </div>
+                </Link>
+              </li>
             ))
           ) : (
             <div className='flex-col items-center justify-center col-span-1 sm:col-span-1 md:col-span-2 lg:col-span-3'>
