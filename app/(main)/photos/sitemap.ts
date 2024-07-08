8f83e17a56b8dee0e51171567a312d3ff2b4cf8e
Eamon Gordon <ekeokigordon@icloud.com>
2024-07-08 16:36:04 -0700

    refactor: Update sitemap generation for articles, localities, minerals, and photos
    This commit updates the sitemap generation for articles, localities, minerals, and photos. The `sitemap.ts` files in the respective directories have been modified to use the `VERCEL_URL` environment variable instead of `NEXT_PUBLIC_VERCEL_URL` for the `baseUrl`. This change ensures that the sitemap URLs are correctly generated for the website. By using the `VERCEL_URL`, the sitemaps will reflect the actual production URL, improving SEO and search engine indexing.

--- app/(main)/photos/sitemap.ts
+++ app/(main)/photos/sitemap.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from 'next'
 import prisma from '@/lib/prisma'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-    const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL;
+    const baseUrl = process.env.VERCEL_URL;
     const pages = await prisma.photo.findMany({
         select: {
             id: true
