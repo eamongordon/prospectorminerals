29b15357130e44fdf030dfb2ba629b2b2f8c9893
Eamon Gordon <ekeokigordon@icloud.com>
2024-07-08 16:31:12 -0700

    feat: Add sitemap generation for whole site, articles, localities, minerals, and photos
    This commit adds new files and functionality to generate sitemaps for articles, localities, minerals, and photos. Each sitemap includes the corresponding URLs for the respective pages. This will improve SEO and make it easier for search engines to crawl and index the website.

---
+++ app/(main)/localities/sitemap.ts
@@ -0,0 +1,20 @@
+import { MetadataRoute } from 'next'
+import prisma from '@/lib/prisma'
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+    const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL;
+    const pages = await prisma.locality.findMany({
+        select: {
+            slug: true
+        }
+    });
+
+    return [
+        {
+            url: `${baseUrl}/localities`
+        },
+        ...pages.map((page) => ({
+            url: `${baseUrl}/localities/${page.slug}`
+        })),
+    ]
+}
