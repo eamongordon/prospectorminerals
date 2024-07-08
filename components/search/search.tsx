cfc50bb061b6926b44248e00e90950f728bdc6d9
Eamon Gordon <ekeokigordon@icloud.com>
2024-07-08 10:30:14 -0700

    refactor: add skeleton and loading state to search component

--- components/search/search.tsx
+++ components/search/search.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useRef, useState } from "react";
 import { fetchMinerals, fetchLocalities, fetchPhotos, fetchPosts } from "@/lib/actions";
 import BlurImage from "../blur-image";
-import { Chip, Input } from "@nextui-org/react";
+import { Button, Chip, Input, Skeleton } from "@nextui-org/react";
 import { Search as MagnifyingGlassIcon } from "lucide-react";
 import { useDebounce } from "use-debounce";
 import Link from "next/link";
@@ -16,9 +16,11 @@ export default function Search({ isHero }: { isHero?: boolean }) {
     const [searchTerm, setSearchTerm] = useState("");
     const [results, setResults] = useState<SearchResult[]>([]);
     const [query] = useDebounce(searchTerm, 500);
+    const [noResultsLoading, setNoResultsLoading] = useState(false);
+    const [resultsLoading, setResultsLoading] = useState(false);
     const initialLoad = useRef(false);
     useEffect(() => {
-        if (searchTerm) {
+        if (searchTerm || initialLoad.current) {
             fetchResults();
         } else {
             if (initialLoad.current) {
@@ -27,6 +29,7 @@ export default function Search({ isHero }: { isHero?: boolean }) {
         }
     }, [query]);
     function fetchResults() {
+        console.log("fetchRes")
         initialLoad.current = true;
         Promise.all([
             fetchMinerals({ filterObj: { name: query }, limit: 3, fieldset: 'display' }),
@@ -49,6 +52,12 @@ export default function Search({ isHero }: { isHero?: boolean }) {
             });
             allResults.length = 3;
             setResults(allResults);
+            if (resultsLoading) {
+                setResultsLoading(false);
+            }
+            if (noResultsLoading) {
+                setNoResultsLoading(false);
+            }
         });
     }
     return (
@@ -62,41 +71,65 @@ export default function Search({ isHero }: { isHero?: boolean }) {
                     classNames={{ base: "w-full" }}
                     value={searchTerm || ""}
                     isClearable={searchTerm ? true : false}
-                    onValueChange={setSearchTerm}
+                    onValueChange={(value) => {setResultsLoading(true); setSearchTerm(value)}}
                     endContent={
                         searchTerm ? (null) : (<><div className='h-full flex items-center'><MagnifyingGlassIcon /></div></>)
                     }
                 />
                 <div className="relative">
-                <div className={`${isHero ? "bg-white dark:bg-zinc-900" : "sm:bg-white sm:dark:bg-zinc-900"} absolute w-full rounded-lg p-5 ${results.length > 0 ? "" : "hidden"}`}>
-                    {results.length > 0 ?
-                        (
-                            <ul className="flex flex-col rounded-lg gap-4">
-                                {results.map((result) => (
-                                    <li key={result.slug} className="hover:opacity-70">
-                                        <Link className="flex flex-row gap-4" href={result.type === "Mineral" ? `/minerals/${result.slug}` : result.type === "Photo" ? `/photos/${result.slug}` : result.type === "Locality" ? `/localities/${result.slug}` : `/articles/${result.slug}`}>
-                                            <BlurImage
-                                                src={result.image!}
-                                                alt={result.name}
-                                                blurDataURL={result.imageBlurhash}
-                                                width={100}
-                                                height={100}
-                                                className="rounded-lg object-cover aspect-[3/2]"
-                                            />
-                                            <div className="flex flex-col">
-                                                <div className="justify-center items-center">
-                                                    <p className="text-lg font-semibold">{result.name}</p>
-                                                </div>
-                                                <Chip size="md">{result.type}</Chip>
+                    <div className={`${isHero ? "bg-white dark:bg-zinc-900" : "sm:bg-white sm:dark:bg-zinc-900"} absolute w-full rounded-lg ${!initialLoad.current && !resultsLoading ? "" : "p-5"}`}>
+                        {resultsLoading ? (
+                            <div className="flex flex-col rounded-lg gap-4">
+                                <>
+                                    {[...Array(3)].map((_, index) => (
+                                        <div key={index} className="flex flex-row gap-4 h-[66.7px]">
+                                            <Skeleton className="w-[100px] aspect-3/2 rounded-lg shrink-0" />
+                                            <div className="flex flex-col justify-center w-full gap-2">
+                                                <Skeleton className="h-[20px] w-full rounded-lg" />
+                                                <Skeleton className="h-[25px] w-[72px] rounded-full" />
                                             </div>
-                                        </Link>
-                                    </li>
-                                ))}
-                            </ul>
-                        )
-                        : (<></>)
-                    }
-                </div>
+                                        </div>
+                                    ))}
+                                </>
+                            </div>
+                        ) : (<></>)}
+                        {!resultsLoading && results.length && results[0] ?
+                            (
+                                <ul className="flex flex-col rounded-lg gap-4">
+                                    {results.map((result) => (
+                                        <li key={result.slug} className="hover:opacity-70">
+                                            <Link className="flex flex-row gap-4" href={result.type === "Mineral" ? `/minerals/${result.slug}` : result.type === "Photo" ? `/photos/${result.slug}` : result.type === "Locality" ? `/localities/${result.slug}` : `/articles/${result.slug}`}>
+                                                <BlurImage
+                                                    src={result.image!}
+                                                    alt={result.name}
+                                                    blurDataURL={result.imageBlurhash}
+                                                    width={100}
+                                                    height={100}
+                                                    className="rounded-lg object-cover aspect-[3/2]"
+                                                />
+                                                <div className="flex flex-col justify-center">
+                                                    <div className="justify-center items-center">
+                                                        <p className="text-lg font-semibold">{result.name}</p>
+                                                    </div>
+                                                    <Chip size="md">{result.type}</Chip>
+                                                </div>
+                                            </Link>
+                                        </li>
+                                    ))}
+                                </ul>
+                            ) : (<></>)
+                        }
+                        {initialLoad.current && !resultsLoading && !results[0] ? (
+                            <div className='flex-col items-center justify-center'>
+                                <p className='w-full text-center'>No Results Found. Try adjusting your filters.</p>
+                                <div className='flex items-center justify-center py-4'>
+                                    <Button className="flex" isLoading={noResultsLoading} onClick={() => { setNoResultsLoading(true); setSearchTerm(""); }}>
+                                        Clear Filters
+                                    </Button>
+                                </div>
+                            </div>
+                        ) : (<></>)}
+                    </div>
                 </div>
             </div>
         </div>
