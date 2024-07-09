8bf698dd012d9023a1d92fa5f600ba902c404265
Eamon Gordon <ekeokigordon@icloud.com>
2024-07-08 19:23:06 -0700

    refactor: Update sitemap generation to use vercel site url
    Use process.env.VERCEL_PROJECT_PRODUCTION_URL

--- app/(main)/articles/sitemap.ts
+++ app/(main)/articles/sitemap.ts
@@ -2,14 +2,14 @@ import { MetadataRoute } from 'next'
 import prisma from '@/lib/prisma'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-    const baseUrl = process.env.VERCEL_URL;
+    const baseUrl = `https://www.${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
     const pages = await prisma.post.findMany({
         select: {
             slug: true,
             updatedAt: true
         }
     });
-
+    console.log(baseUrl)
     return [
         {
             url: `${baseUrl}/articles`
