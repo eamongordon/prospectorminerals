3e58965fe6070a28361127a1003bce18004bcacf
Eamon Gordon <ekeokigordon@icloud.com>
2024-07-08 18:25:28 -0700

    refactor: Add redirects for post paths and old mineral and locality pages

--- next.config.js
+++ next.config.js
@@ -9,7 +9,46 @@ const nextConfig = {
             { hostname: "avatars.githubusercontent.com" },
             { hostname: "www.google.com" },
         ]
-    }
+    },
+    async redirects() {
+        return [
+            {
+                source: '/post/:path*',
+                destination: '/articles/:path*',
+                permanent: true,
+            },
+            {
+                source: '/learn',
+                destination: '/#explore',
+                permanent: false,
+            },
+            {
+                source: '/about-minerals',
+                destination: '/minerals',
+                permanent: true,
+            },
+            {
+                source: '/mineral-database',
+                destination: '/minerals',
+                permanent: true,
+            },
+            {
+                source: '/about-localities',
+                destination: '/localities',
+                permanent: true,
+            },
+            {
+                source: '/locality-database',
+                destination: '/localities',
+                permanent: true,
+            },
+            {
+                source: '/locality-map',
+                destination: '/localities',
+                permanent: true,
+            }
+        ]
+    },
 }
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
