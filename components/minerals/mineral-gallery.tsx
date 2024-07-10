a2d38f2e9aa983c642e172c6f19870083f8ee386
Eamon Gordon <ekeokigordon@icloud.com>
2024-07-09 17:39:12 -0700

    refactor: Optimize image rendering in MineralGallery component
    Improve the performance and user experience of the MineralGallery component by optimizing the image rendering. This includes adding width, height, and quality attributes to the image elements, as well as providing alt text for accessibility. These changes ensure that the images are displayed correctly and enhance the overall visual presentation of the component.

--- components/minerals/mineral-gallery.tsx
+++ components/minerals/mineral-gallery.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import Image from 'next/image';
+import BlurImage from '../blur-image';
 
 export type GalleryItem = {
     title?: string;
     caption?: string;
     image?: string;
+    blurDataURL?: string;
 };
 
 export default function Gallery({ data }: { data: GalleryItem[] }) {
@@ -15,50 +17,52 @@ export default function Gallery({ data }: { data: GalleryItem[] }) {
                 className={`group ${data.length >= 3 ? "relative rounded-md row-span-2 col-span-2 flex flex-col items-center justify-center" : "relative rounded-md row-span-1 col-span-1 flex flex-col items-center justify-center"}`}
                 id="image1"
             >
-                <Image
-                    className={"group-hover:brightness-50 group-hover:blur-sm rounded-xl z-0"}
+                <BlurImage
+                    width={750}
+                    height={500}
+                    alt={data[0]?.title || "Photo"}
+                    className={"group-hover:brightness-50 group-hover:blur-sm rounded-xl z-0 w-full h-full object-cover absolute"}
                     src={data[0]?.image || "/Amazonite-106_horiz.jpeg"}
-                    alt="Sample Image"
-                    objectFit='cover'
-                    fill={true}
+                    blurDataURL={data[0]?.blurDataURL}
                 />
-                <>
+                <div className='absolute'>
                     <h3 className="text-center group-hover:block hidden text-md sm:text-xl z-10 font-medium text-white">{data[0]?.title}</h3>
                     <p className="text-center group-hover:block hidden text-sm sm:text-md z-10 text-white">{data[0]?.caption}</p>
-                </>
+                </div>
             </div>
             <div
                 className={`group ${data.length >= 3 ? "relative row-span-1 col-span-1 flex flex-col items-center justify-center" : data.length === 2 ? "relative row-span-1 col-span-1 flex flex-col items-center justify-center" : "hidden"}`}
                 id="image2"
             >
-                <Image
-                    className="group-hover:brightness-50 group-hover:blur-sm rounded-xl"
-                    src={data[1]?.image || "/Cavansite-45.jpeg"}
-                    fill={true}
-                    objectFit='cover'
-                    alt="Sample Image"
+                <BlurImage
+                    width={625}
+                    height={475}
+                    alt={data[1]?.title || "Photo"}
+                    className={"group-hover:brightness-50 group-hover:blur-sm rounded-xl z-0 w-full h-full object-cover absolute"}
+                    src={data[1]?.image || "/Amazonite-106_horiz.jpeg"}
+                    blurDataURL={data[1]?.blurDataURL}
                 />
-
-                <>
+                <div className='absolute'>
                     <h3 className="text-center group-hover:block hidden text-md sm:text-xl z-10 font-medium text-white">{data[1]?.title}</h3>
                     <p className="text-center group-hover:block hidden text-sm sm:text-md z-10 text-white">{data[1]?.caption}</p>
-                </>
+                </div>
             </div>
             <div
                 className={`group ${data.length >= 3 ? "relative row-span-1 col-span-1 flex flex-col items-center justify-center" : "hidden"}`}
                 id="image3"
             >
-                <Image
-                    className={"group-hover:brightness-50 group-hover:blur-sm rounded-xl z-0"}
-                    src={data[2]?.image || "/Krohnkite-110_horiz.jpeg"}
-                    alt="Sample Image"
-                    fill={true}
-                    objectFit='cover'
+                <BlurImage
+                    width={400}
+                    height={225}
+                    alt={data[2]?.title || "Photo"}
+                    className={"group-hover:brightness-50 group-hover:blur-sm rounded-xl z-0 w-full h-full object-cover absolute"}
+                    src={data[2]?.image || "/Amazonite-106_horiz.jpeg"}
+                    blurDataURL={data[2]?.blurDataURL}
                 />
-                <>
+                <div className='absolute'>
                     <h3 className="text-center group-hover:block hidden text-md sm:text-xl z-10 font-medium text-white">{data[2]?.title}</h3>
                     <p className="text-center group-hover:block hidden text-sm sm:text-md z-10 text-white">{data[2]?.caption}</p>
-                </>
+                </div>
             </div>
         </div>
     )
