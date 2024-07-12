5e3f19b0ff54d36d32f55ac6856bbd649535a1f1
Eamon Gordon <ekeokigordon@icloud.com>
2024-07-12 15:41:15 -0700

    attempt to fix redirect loop production auth

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,10 @@ import authConfig from "@/lib/auth/auth.config"
 export const { auth } = NextAuth(authConfig)
 
 export default auth((req) => {
+  console.log("AUTH REQ");
+  console.log(req.auth);
+  console.log("ALL REQ");
+  console.log(req);
   if (!req.auth) {
     const url = req.url.replace(req.nextUrl.pathname, `/login?callbackUrl=${encodeURIComponent(req.url)}`)
     return Response.redirect(url)
