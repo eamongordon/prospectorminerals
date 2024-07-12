326ce32c05ceb1886180992c7de4a776db3fb599
Eamon Gordon <ekeokigordon@icloud.com>
2024-07-12 16:33:34 -0700

    attept fix 2

--- lib/auth/auth.config.ts
+++ lib/auth/auth.config.ts
@@ -1,8 +1,4 @@
 import { NextAuthConfig } from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import prisma from "@/lib/prisma";
 
 export default {
     providers: []
