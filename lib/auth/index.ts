95682fbb5d476093595bab37c641d9dd7b5b17d5
Eamon Gordon <ekeokigordon@icloud.com>
2024-07-12 16:33:07 -0700

    attempt fix

--- lib/auth/index.ts
+++ lib/auth/index.ts
@@ -2,16 +2,15 @@ import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials";
 import Nodemailer from "next-auth/providers/nodemailer";
 import { createTransport } from "nodemailer";
-import authConfig from "./auth.config";
 import prisma from "@/lib/prisma";
 import { compare } from "bcrypt";
-
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 const VERCEL_DEPLOYMENT = !!`https://www.${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  ...authConfig,
   providers: [
-    ...authConfig.providers,
     CredentialsProvider({
       credentials: {
         email: { label: "Email", type: "email" },
@@ -24,7 +23,7 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
         }
         const user = await prisma.user.findUnique({
           where: {
-            email : email as string,
+            email: email as string,
           },
         });
         // if user doesn't exist or password doesn't match
@@ -62,7 +61,109 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
         }
       }
     }),
-  ]
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      profile(profile) {
+        return {
+          id: profile.sub,
+          name: profile.name || profile.email,
+          first_name: profile.given_name,
+          last_name: profile.family_name,
+          email: profile.email,
+          image: profile.picture,
+        }
+      }
+    }),
+    GitHubProvider({
+      clientId: process.env.AUTH_GITHUB_ID as string,
+      clientSecret: process.env.AUTH_GITHUB_SECRET as string,
+      profile(profile) {
+        return {
+          id: profile.id.toString(),
+          name: profile.name || profile.login,
+          gh_username: profile.login,
+          email: profile.email,
+          image: profile.avatar_url,
+        };
+      },
+    }),
+  ],
+  pages: {
+    signIn: `/login`,
+    verifyRequest: `/login`,
+    error: "/login", // Error code passed in query string as ?error=
+  },
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  /*
+  cookies: {
+    sessionToken: {
+      name: `${VERCEL_DEPLOYMENT ? "__Secure-" : ""}next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: "lax",
+        path: "/",
+        // When working on localhost, the cookie domain must be omitted entirely (https://stackoverflow.com/a/1188145)
+        domain: VERCEL_DEPLOYMENT
+          ? `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`
+          : undefined,
+        secure: VERCEL_DEPLOYMENT,
+      },
+    },
+  },
+  */
+  theme: {
+    colorScheme: "auto", // "auto" | "dark" | "light"
+    brandColor: "#666565", // Hex color code
+    //DOMAIN_UPDATE
+    logo: "https://prospectorminerals.vercel.app/_next/image?url=%2FPM-Favicon-New-Square.png&w=128&q=75", // Absolute URL to image
+    buttonText: "#FFFFFF" // Hex color code
+  },
+  callbacks: {
+    jwt: async ({ token, user, trigger, session }) => {
+      if (trigger === "update") {
+        const sessionKeyList = Object.keys(session);
+        sessionKeyList.forEach(async (key) => {
+          token[key] = session[key];
+          //@ts-expect-error;
+          if (token?.user && token?.user[key]) {
+            if (key !== 'password') {
+              //@ts-expect-error;
+              token.user[key] = session[key];
+            }
+          }
+        });
+      }
+      return token;
+    },
+    session: async ({ session, token }) => {
+      session.user = {
+        ...session.user,
+        // @ts-expect-error
+        id: token.sub,
+        // @ts-expect-error
+        username: token?.user?.username || token?.user?.gh_username,
+      };
+      return session;
+    },
+    signIn: async ({ user, profile }) => {
+      if (!profile) {
+        const userExists = await prisma.user.findUnique({
+          where: {
+            email: user.email || undefined
+          },
+        });
+        if (userExists) {
+          return true;   //if the email exists in the User collection, email them a magic login link
+        } else {
+          return false;
+        }
+      } else {
+        return true;
+      }
+    },
+  }
 });
 
 interface Theme {
--- middleware.ts
+++ middleware.ts
@@ -4,10 +4,8 @@ import authConfig from "@/lib/auth/auth.config"
 export const { auth } = NextAuth(authConfig)
 
 export default auth((req) => {
-  console.log("AUTH REQ");
+  console.log(req.url);
   console.log(req.auth);
-  console.log("ALL REQ");
-  console.log(req);
   if (!req.auth) {
     const url = req.url.replace(req.nextUrl.pathname, `/login?callbackUrl=${encodeURIComponent(req.url)}`)
     return Response.redirect(url)
