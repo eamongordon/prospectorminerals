7d69eb361af3f2eb6a1314dccfe830dd85ca767b
Eamon Gordon <ekeokigordon@icloud.com>
2024-07-12 19:44:24 -0700

    refactor: clean up auth config, add AUTH_URL to resolve middleware issues in production

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,6 @@ import authConfig from "@/lib/auth/auth.config"
 export const { auth } = NextAuth(authConfig)
 
 export default auth((req) => {
-  console.log(req.url);
-  console.log(req.auth);
   if (!req.auth) {
     const url = req.url.replace(req.nextUrl.pathname, `/login?callbackUrl=${encodeURIComponent(req.url)}`)
     return Response.redirect(url)
