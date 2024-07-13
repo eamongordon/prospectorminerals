6ade8a97ab077162aac77d1777863c2ab0acb506
Eamon Gordon <ekeokigordon@icloud.com>
2024-07-13 11:54:12 -0700

    refactor: Update sitemap generation for articles, localities, minerals, and photos
    Update the sitemap generation for the articles, localities, minerals, and photos sections of the application. The `baseUrl` variable is now set using the `NEXT_PUBLIC_VERCEL_URL` environment variable instead of the `NEXT_PUBLIC_VERCEL_URL` variable. This change ensures that the correct base URL is used for generating the sitemap.

--- app/(main)/articles/sitemap.ts
+++ app/(main)/articles/sitemap.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from 'next'
 import prisma from '@/lib/prisma'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-    const baseUrl = `https://www.${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
+    const baseUrl = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
     const pages = await prisma.post.findMany({
         select: {
             slug: true,
