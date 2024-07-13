1af46c0929a21df3d5aaa513b4f99929e374ec13
Eamon Gordon <ekeokigordon@icloud.com>
2024-07-13 13:58:28 -0700

    fix: finish removing loading dots

--- components/modal/login.tsx
+++ components/modal/login.tsx
@@ -1,15 +1,9 @@
 "use client";
 
-import { toast } from "sonner";
 //import { createContact } from "@/lib/functions/contacts/contacts";
-import { useRouter } from "next/navigation";
-import LoginForm from "@/components/registration/login-form"
-import { useFormStatus } from "react-dom";
-import { cn } from "@/lib/utils";
-import LoadingDots from "@/components/icons/loading-dots";
 import { useModal } from "@/components/modal/registration/provider";
-import va from "@vercel/analytics";
-import { useEffect, useState } from "react";
+import LoginForm from "@/components/registration/login-form";
+import { useRouter } from "next/navigation";
 
 export default function LoginModal() {
   const router = useRouter();
