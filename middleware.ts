1c63fc0f0ea130f2f82c7d1e96bd2a0bd66cfaab
Eamon Gordon <ekeokigordon@icloud.com>
2024-07-13 13:05:35 -0700

    refactor: Add roles to user model and update authentication config
    Update the user model to include a `roles` field and modify the authentication configuration to handle user roles. This allows for role-based access control in the application.

--- middleware.ts
+++ middleware.ts
@@ -7,9 +7,14 @@ export default auth((req) => {
   if (!req.auth) {
     const url = req.url.replace(req.nextUrl.pathname, `/login?callbackUrl=${encodeURIComponent(req.url)}`)
     return Response.redirect(url)
+  } else {
+    //Ensure condition yields true when user or roles is undefined
+    if ((req.nextUrl.pathname.startsWith('/manage') || req.nextUrl.pathname.startsWith('/sandbox')) && !(req.auth.user?.roles?.includes('Admin') ?? true)) {
+      return new Response('Forbidden', { status: 403 });
+    }
   }
 })
 
 export const config = {
-  matcher: ["/manage/:path*", "/account/:path*"],
+  matcher: ["/manage/:path*", "/account/:path*", "/sandbox/:path*"],
 }
