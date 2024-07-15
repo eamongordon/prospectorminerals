c4cfb7035444997018875b29dc0b74867a970cec
Eamon Gordon <ekeokigordon@icloud.com>
2024-07-15 14:08:47 -0700

    refactor: clean up post management formatting
    Remove popsy illustrations, fix links in manage posts component

--- app/(main)/manage/posts/page.tsx
+++ app/(main)/manage/posts/page.tsx
@@ -3,28 +3,24 @@ import Posts from "@/components/manage/posts/posts";
 import { getSession } from "@/lib/auth";
 import { redirect } from "next/navigation";
 
-export default async function SitePosts({
-  params,
-}: {
-  params: { id: string };
-}) {
+export default async function SitePosts() {
 
   const session = await getSession();
   if (!session) {
     redirect("/login");
   }
 
-  const url = `${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`;
+  const url = `${process.env.VERCEL_PROJECT_PRODUCTION_URL ? "https://" + process.env.VERCEL_PROJECT_PRODUCTION_URL : "http://" + "localhost:3001"}/articles`;
 
   return (
-    <>
+    <div className="mx-8">
       <div className="flex flex-col items-center justify-between space-y-4 sm:flex-row sm:space-y-0">
         <div className="flex flex-col items-center space-y-2 sm:flex-row sm:space-x-4 sm:space-y-0">
-          <h1 className="w-60 truncate font-cal text-xl font-bold dark:text-white sm:w-auto sm:text-3xl">
+          <h1 className="w-60 truncate text-xl font-bold dark:text-white sm:w-auto sm:text-3xl">
             All Posts
           </h1>
           <a
-            href={"/"
+            href={url
               /*
               process.env.NEXT_PUBLIC_VERCEL_ENV
                 ? `https://${url}`
@@ -40,7 +36,7 @@ export default async function SitePosts({
         </div>
         <CreatePostButton />
       </div>
-      <Posts siteId={decodeURIComponent(params.id)} />
-    </>
+      <Posts />
+    </div>
   );
 }
