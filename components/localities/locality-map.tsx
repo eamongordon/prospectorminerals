8492ab1a5d70848c8af552f77721a89d44bade3e
Eamon Gordon <ekeokigordon@icloud.com>
2024-07-16 12:16:58 -0700

    refactor: Add lazy loading for react leaflet, fix window undefined error
    Update the `localities-page-layout.tsx` component to import the `Skeleton` component from `@nextui-org/react` and wrap the `LocalityMap` component in a `Suspense` component. This enables lazy loading for images on the localities page, improving page load performance.

--- components/localities/locality-map.tsx
+++ components/localities/locality-map.tsx
@@ -1,78 +1,97 @@
 "use client"
 
 import { Card, CardFooter, Skeleton } from "@nextui-org/react";
-import L from 'leaflet';
-import "leaflet-defaulticon-compatibility";
-import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import 'leaflet/dist/leaflet.css';
 import { Work_Sans } from 'next/font/google';
 import Link from 'next/link';
-import { Suspense } from 'react';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { useEffect, useMemo, useState } from 'react';
 import BlurImage from '../blur-image';
 import './popup-styles.css';
 import type { LocalityDisplayFieldsetComponent } from '@/types/prisma';
+import dynamic from "next/dynamic";
 
 const inter = Work_Sans({ subsets: ['latin'] })
 
 export default function LocalityMap({ localities, center, zoom }: { localities: LocalityDisplayFieldsetComponent[], center: [number, number], zoom: number }) {
-    const singleLocalityKnownIcon = L.icon({
-        iconUrl: '/localities/PM-Single-Locality-Pin_Light.png', // Replace with your icon's path
-        iconSize: [35, 35], // Adjust based on your icon's dimensions
-        iconAnchor: [17.5, 35], // Adjust to ensure the icon is centered on its coordinates
-    });
+    // Dynamically import React Leaflet components with SSR disabled
+    const MapContainer = useMemo(() => dynamic(() => import('react-leaflet').then(mod => mod.MapContainer), {
+        ssr: false,
+        loading: () => <Skeleton className='w-full max-h-[400px] aspect-[5/3]'></Skeleton>
+    }), []);
 
-    const singleLocalityEstimatedIcon = L.icon({
-        iconUrl: '/localities/PM-Single-Locality-Pin-Dark.png', // Replace with your icon's path
-        iconSize: [35, 35], // Adjust based on your icon's dimensions
-        iconAnchor: [17.5, 35], // Adjust to ensure the icon is centered on its coordinates
-    });
+    const TileLayer = useMemo(() => dynamic(() => import('react-leaflet').then(mod => mod.TileLayer), {
+        ssr: false,
+    }), []);
 
-    const groupLocalityKnownIcon = L.icon({
-        iconUrl: '/localities/PM-Group-Locality-Pin_Light.png', // Replace with your icon's path
-        iconSize: [35, 35], // Adjust based on your icon's dimensions
-        iconAnchor: [17.5, 35], // Adjust to ensure the icon is centered on its coordinates
-    });
+    const Marker = useMemo(() => dynamic(() => import('react-leaflet').then(mod => mod.Marker), {
+        ssr: false,
+    }), []);
 
-    const groupLocalityEstimatedIcon = L.icon({
-        iconUrl: '/localities/PM-Group-Locality-Pin_Dark.png', // Replace with your icon's path
-        iconSize: [35, 35], // Adjust based on your icon's dimensions
-        iconAnchor: [17.5, 35], // Adjust to ensure the icon is centered on its coordinates
-    });
+    const Popup = useMemo(() => dynamic(() => import('react-leaflet').then(mod => mod.Popup), {
+        ssr: false,
+    }), []);
+
+    const [singleLocalityKnownIcon, setSingleLocalityKnownIcon] = useState(null);
+    const [singleLocalityEstimatedIcon, setSingleLocalityEstimatedIcon] = useState(null);
+    const [groupLocalityKnownIcon, setGroupLocalityKnownIcon] = useState(null);
+    const [groupLocalityEstimatedIcon, setGroupLocalityEstimatedIcon] = useState(null);
+
+    useEffect(() => {
+        const L = require('leaflet');
+        setSingleLocalityKnownIcon(L.icon({
+            iconUrl: '/localities/PM-Single-Locality-Pin_Light.png',
+            iconSize: [35, 35],
+            iconAnchor: [17.5, 35],
+        }));
+        setSingleLocalityEstimatedIcon(L.icon({
+            iconUrl: '/localities/PM-Single-Locality-Pin-Dark.png',
+            iconSize: [35, 35],
+            iconAnchor: [17.5, 35],
+        }));
+        setGroupLocalityKnownIcon(L.icon({
+            iconUrl: '/localities/PM-Group-Locality-Pin_Light.png',
+            iconSize: [35, 35],
+            iconAnchor: [17.5, 35],
+        }));
+        setGroupLocalityEstimatedIcon(L.icon({
+            iconUrl: '/localities/PM-Group-Locality-Pin_Dark.png',
+            iconSize: [35, 35],
+            iconAnchor: [17.5, 35],
+        }));
+    }, []);
+
     return (
-        <Suspense fallback={<Skeleton className='h-[400px] w-full'></Skeleton>}>
-            <MapContainer
-                className='w-full max-h-[400px] aspect-[5/3] z-0 py-0'
-                center={center} zoom={zoom} scrollWheelZoom={false}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                {localities.map((locality) => {
-                    return (<Marker key={locality.id} position={[Number(locality.latitude), Number(locality.longitude)]} icon={locality.type === 'Single' ? locality.coordinates_known ? singleLocalityKnownIcon : singleLocalityEstimatedIcon : locality.coordinates_known ? groupLocalityKnownIcon : groupLocalityEstimatedIcon} >
-                        <Popup className={`${inter.className} w-[200px]`} offset={[0, -21]}>
-                            <div key={locality.id}>
-                                <Link href={`/localities/${locality.slug}`}>
-                                    <Card isFooterBlurred className="w-full">
-                                        <BlurImage
-                                            width={300}
-                                            height={300}
-                                            quality={50}
-                                            alt={locality.name || "Photo"}
-                                            className="object-cover w-full h-full scale-125 -translate-y-6 aspect-[5/4]"
-                                            src={locality.photos.length > 0 && locality.photos[0].image ? locality.photos[0].image : "/Amazonite-106_horiz.jpeg"}
-                                            blurDataURL={locality.photos.length > 0 && locality.photos[0].imageBlurhash ? locality.photos[0].imageBlurhash : undefined}
-                                        />
-                                        <CardFooter className="absolute bg-white/30 bottom-0 z-10 flex justify-center items-center">
-                                            <p className="mx-auto text-white text-lg font-semibold locality-card-text">{locality.name}</p>
-                                        </CardFooter>
-                                    </Card >
-                                </Link>
-                            </div>
-                        </Popup>
-                    </Marker>)
-                })}
-            </MapContainer>
-        </Suspense>
+        <MapContainer
+            className='w-full max-h-[400px] aspect-[5/3] z-0 py-0'
+            center={center} zoom={zoom} scrollWheelZoom={false}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {localities.map((locality) => {
+                return (<Marker key={locality.id} position={[Number(locality.latitude), Number(locality.longitude)]} icon={locality.type === 'Single' ? locality.coordinates_known ? singleLocalityKnownIcon! : singleLocalityEstimatedIcon! : locality.coordinates_known ? groupLocalityKnownIcon! : groupLocalityEstimatedIcon!} >
+                    <Popup className={`${inter.className} w-[200px]`} offset={[0, -21]}>
+                        <div key={locality.id}>
+                            <Link href={`/localities/${locality.slug}`}>
+                                <Card isFooterBlurred className="w-full">
+                                    <BlurImage
+                                        width={300}
+                                        height={300}
+                                        quality={50}
+                                        alt={locality.name || "Photo"}
+                                        className="object-cover w-full h-full scale-125 -translate-y-6 aspect-[5/4]"
+                                        src={locality.photos.length > 0 && locality.photos[0].image ? locality.photos[0].image : "/Amazonite-106_horiz.jpeg"}
+                                        blurDataURL={locality.photos.length > 0 && locality.photos[0].imageBlurhash ? locality.photos[0].imageBlurhash : undefined}
+                                    />
+                                    <CardFooter className="absolute bg-white/30 bottom-0 z-10 flex justify-center items-center">
+                                        <p className="mx-auto text-white text-lg font-semibold locality-card-text">{locality.name}</p>
+                                    </CardFooter>
+                                </Card >
+                            </Link>
+                        </div>
+                    </Popup>
+                </Marker>)
+            })}
+        </MapContainer>
     )
 }
