e0c806a387a12cc49ecfaa5a24d79e65d8c8ecb4
Eamon Gordon <ekeokigordon@icloud.com>
2024-07-18 19:34:35 -0700

    refactor: Improve article page layout and styling

--- app/(main)/articles/[slug]/page.tsx
+++ app/(main)/articles/[slug]/page.tsx
@@ -46,28 +46,26 @@ export default async function SitePostPage({
   return (
     <>
       <main className="my-5">
-        <div className="flex flex-col items-center justify-center">
-          <div className="m-auto w-full text-center md:w-7/12">
-            <p className="m-auto mb-5 w-10/12 text-sm text-stone-500 dark:text-stone-400 md:text-base">
-              {toDateString(data.publishedAt || data.createdAt)}
-            </p>
-            <h1 className="mb-10 font-title text-3xl font-bold text-stone-800 dark:text-white md:text-6xl">
-              {data.title}
-            </h1>
-            <p className="mb-10 text-md m-auto w-10/12 text-stone-600 dark:text-stone-400 md:text-lg">
-              {data.description}
-            </p>
-          </div>
+        <div className="m-auto mb-10 gap-5 sm:gap-7 flex flex-col items-center justify-center md:w-7/12 text-center">
+          <p className="m-auto w-10/12 text-sm text-stone-500 dark:text-stone-400 md:text-base">
+            {toDateString(data.publishedAt || data.createdAt)}
+          </p>
+          <h1 className="font-title text-3xl font-bold text-stone-800 dark:text-white md:text-6xl">
+            {data.title}
+          </h1>
+          <p className="text-md m-auto w-10/12 text-stone-600 dark:text-stone-400 md:text-lg">
+            {data.description}
+          </p>
         </div>
         <div className="relative m-auto mb-10 h-80 w-full max-w-screen-lg overflow-hidden md:h-150 md:w-5/6 md:rounded-2xl lg:w-2/3">
           <BlurImage
-            alt={data.title ?? "Post image"}
+            alt={data.title ?? "Article Image"}
             width={1200}
             height={630}
             className="h-full w-full object-cover"
             placeholder="blur"
             blurDataURL={data.imageBlurhash ?? placeholderBlurhash}
-            src={data.image ?? "/placeholder.png"}
+            src={data.image ?? "/Fluorite-164_horiz-Optimized.jpg"}
           />
         </div>
         <MDX source={data.transformedContent} />
--- app/(main)/manage/posts/page.tsx
+++ app/(main)/manage/posts/page.tsx
@@ -15,19 +15,13 @@ export default async function SitePosts() {
 
   return (
     <div className="mx-8">
-      <div className="flex flex-col items-center justify-between space-y-4 sm:flex-row sm:space-y-0">
+      <div className="flex flex-col items-center justify-between my-4 gap-2 sm:flex-row">
         <div className="flex flex-col items-center space-y-2 sm:flex-row sm:space-x-4 sm:space-y-0">
           <h1 className="w-60 truncate text-xl font-bold dark:text-white sm:w-auto sm:text-3xl">
             All Posts
           </h1>
           <a
-            href={url
-              /*
-              process.env.NEXT_PUBLIC_VERCEL_ENV
-                ? `https://${url}`
-                : `http://${data.subdomain}.localhost:3000`
-                */
-            }
+            href={url}
             target="_blank"
             rel="noreferrer"
             className="truncate rounded-md bg-stone-100 px-2 py-1 text-sm font-medium text-stone-600 transition-colors hover:bg-stone-200 dark:bg-stone-800 dark:text-stone-400 dark:hover:bg-stone-700"
