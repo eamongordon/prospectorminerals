d06e8ebf45dc679dec65f971de09b6fa01c6444b
Eamon Gordon <ekeokigordon@icloud.com>
2024-07-18 19:05:17 -0700

    refactor: Add login and signup URLs to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,13 +2,18 @@ import { MetadataRoute } from 'next'
 import { baseUrl } from '@/lib/utils';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-
     return [
         {
             url: baseUrl
         },
         {
             url: `${baseUrl}/policy`
+        },
+        {
+            url: `${baseUrl}/login`
+        },
+        {
+            url: `${baseUrl}/signup`
         }
     ]
 }
