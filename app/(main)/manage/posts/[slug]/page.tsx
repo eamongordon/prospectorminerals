44cb020cced0a24cb7d600b6afd39547652ff172
Eamon Gordon <ekeokigordon@icloud.com>
2024-07-30 12:26:26 -0700

    refactor: add saving button for editor

--- app/(main)/manage/posts/[slug]/page.tsx
+++ app/(main)/manage/posts/[slug]/page.tsx
@@ -68,39 +68,3 @@ export default async function PostPage({ params }: { params: { slug: string } })
     </>
   );
 }
-
-/*
-    <div className="flex max-w-screen-xl flex-col space-y-12 p-6">
-      <div className="flex flex-col space-y-6">
-        <h1 className="text-3xl font-bold dark:text-white">
-          Post Settings
-        </h1>
-        <Form
-          title="Post Slug"
-          description="The slug is the URL-friendly version of the name. It is usually all lowercase and contains only letters, numbers, and hyphens."
-          helpText="Please use a slug that is unique to this post."
-          inputAttrs={{
-            name: "slug",
-            type: "text",
-            defaultValue: data?.slug!,
-            placeholder: "slug",
-          }}
-          handleSubmit={updatePostMetadata}
-        />
-
-        <Form
-          title="Thumbnail image"
-          description="The thumbnail image for your post. Accepted formats: .png, .jpg, .jpeg"
-          helpText="Max file size 50MB. Recommended size 1200x630."
-          inputAttrs={{
-            name: "image",
-            type: "file",
-            defaultValue: data?.image!,
-          }}
-          handleSubmit={updatePostMetadata}
-        />
-
-        <DeletePostForm postName={data?.title!} />
-      </div>
-    </div>
-*/
