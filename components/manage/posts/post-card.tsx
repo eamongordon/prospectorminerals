a03ee6ff8f326a42df25b860334209406df86eab
Eamon Gordon <ekeokigordon@icloud.com>
2024-07-30 11:55:04 -0700

    refactor: Update links to use Next.js Link component

--- components/manage/posts/post-card.tsx
+++ components/manage/posts/post-card.tsx
@@ -42,7 +42,7 @@ export default function PostCard({
         </div>
       </Link>
       <div className="absolute bottom-4 flex w-full px-4">
-        <a
+        <Link
           href={url
             /*
             process.env.NEXT_PUBLIC_VERCEL_ENV
@@ -55,7 +55,7 @@ export default function PostCard({
           className="truncate rounded-md bg-stone-100 px-2 py-1 text-sm font-medium text-stone-600 transition-colors hover:bg-stone-200 dark:bg-stone-800 dark:text-stone-400 dark:hover:bg-stone-700"
         >
           {url} ↗
-        </a>
+        </Link>
       </div>
     </div>
   );
