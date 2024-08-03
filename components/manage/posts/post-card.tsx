35a3add82f4742fbd65ede531a17db440a905d06
Eamon Gordon <ekeokigordon@icloud.com>
2024-08-03 11:19:17 -0700

    refactor: clean up comments

--- components/manage/posts/post-card.tsx
+++ components/manage/posts/post-card.tsx
@@ -43,13 +43,7 @@ export default function PostCard({
       </Link>
       <div className="absolute bottom-4 flex w-full px-4">
         <Link
-          href={url
-            /*
-            process.env.NEXT_PUBLIC_VERCEL_ENV
-              ? `https://${url}`
-              : `http://${data.site?.subdomain}.localhost:3000/${data.slug}`
-              */
-          }
+          href={url}
           target="_blank"
           rel="noreferrer"
           className="truncate rounded-md bg-stone-100 px-2 py-1 text-sm font-medium text-stone-600 transition-colors hover:bg-stone-200 dark:bg-stone-800 dark:text-stone-400 dark:hover:bg-stone-700"
