2e86ccde69a09f2b09fc3c26f85a55d61d829938
Eamon Gordon <ekeokigordon@icloud.com>
2024-08-12 11:46:37 -0700

    fix: remove client directive from signup page and unnecessary suspense boundary

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,10 +1,7 @@
 import FormWrapper from "@/components/registration/login-form";
-import { Suspense } from 'react';
 
 export default async function LoginPage() {
   return (
-    <Suspense>
-      <FormWrapper />
-    </Suspense>
+    <FormWrapper />
   );
 }
