abda67f1b932a972846b1e7b4074af0f4dcd17d7
Eamon Gordon <ekeokigordon@icloud.com>
2024-08-22 16:57:56 -0700

    refactor: organize imports layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
-import type { Metadata } from 'next'
-import { Work_Sans } from 'next/font/google'
+import { baseUrl } from '@/lib/utils';
+import { Analytics } from "@vercel/analytics/react";
+import type { Metadata } from 'next';
+import { Work_Sans } from 'next/font/google';
 import './globals.css';
 import { Providers } from './providers';
-import { Analytics } from "@vercel/analytics/react";
-import { baseUrl } from '@/lib/utils';
 
 const workSans = Work_Sans({ subsets: ['latin'] })
 
