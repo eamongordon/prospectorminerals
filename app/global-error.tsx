7360a356e3b583facc976ccbdd8bb00178e89de7
Eamon Gordon <ekeokigordon@icloud.com>
2024-08-22 09:27:09 -0700

    refactor: add workSans const to global error boundary and locality map

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,10 +1,9 @@
 'use client'
-import { Work_Sans } from 'next/font/google'
-import './globals.css';
+
+import { workSansClassName } from '@/lib/utils';
 import { Button } from "@nextui-org/react";
 import Link from "next/link";
-
-const workSans = Work_Sans({ subsets: ['latin'] })
+import './globals.css';
 
 export default function GlobalError({
     error,
@@ -15,7 +14,7 @@ export default function GlobalError({
 }) {
     return (
         <html>
-            <body className={`${workSans.className} bg-gradient-to-b from-[rgb(214,219,220)] to-white text-black dark:bg-gradient-to-b dark:from-black dark:to-black dark:text-white`}>
+            <body className={`${workSansClassName} bg-gradient-to-b from-[rgb(214,219,220)] to-white text-black dark:bg-gradient-to-b dark:from-black dark:to-black dark:text-white`}>
                 <div className="h-screen flex gap-y-5 items-center justify-center">
                     <div className="flex-col items-center justify-center">
                         <h2 className="mb-3 mx-auto">Oops! Something went wrong.</h2>
