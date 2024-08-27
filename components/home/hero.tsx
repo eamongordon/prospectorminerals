53cdd2516bfa9961df398f1c92d147bed0b11b82
Eamon Gordon <ekeokigordon@icloud.com>
2024-08-26 17:40:40 -0700

    fix: center search bar correctly hero

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -18,7 +18,7 @@ export default function Hero() {
                 blurDataURL="data:image/png;base64,/9j/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAmADIDASIAAhEBAxEB/8QAGwAAAgMBAQEAAAAAAAAAAAAAAAQBAgMFBgf/xAAgEAACAgEFAAMAAAAAAAAAAAAAAQIDEQQSEyExIkFR/8QAGAEAAwEBAAAAAAAAAAAAAAAAAAEDAgX/xAAYEQEBAQEBAAAAAAAAAAAAAAAAARExAv/aAAwDAQACEQMRAD8A+e2WN9FqH8kW4W2SoOJ06vT9c+is1uyLQswbqxYNQ4wuqTTOXqql2dS6zpnL1VnbFaekeFfgF+QDGE9PDDItisMTq1GPsm3UZj6UgZ2T2smueRKybcjamePQglMWRbTOdqI9j07lhiF1qbCwYx2AHIgMhvOTiy0JOXoAOitVWsZMbZOPgAKdLyVnfLLFp2tsAKVS8Z8jAAJMP//Z"
                 priority={true}
             />
-            <div className='max-w-screen-xl h-dvh sm:h-auto z-10 w-full flex justify-center items-center'>
+            <div className='max-w-screen-xl h-[calc(100dvh-64px)] sm:h-auto z-10 w-full flex justify-center items-center'>
                 <div className="flex flex-col justify-center items-center w-full px-8 gap-6 min-w-[350px] sm:px-0 sm:w-1/3">
                     <div className='hidden sm:block'>
                         <PMLogo isHero height={65} />
