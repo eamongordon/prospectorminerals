d076e4fa7cb2fc41d3f50cbd39980e1cd4adc1f0
Eamon Gordon <ekeokigordon@icloud.com>
2024-09-03 21:14:50 -0700

    refactor: Remove unused commented out code for login modal

--- components/modal/login.tsx
+++ components/modal/login.tsx
@@ -1,21 +1,11 @@
 "use client";
 
-//import { createContact } from "@/lib/functions/contacts/contacts";
 import { useModal } from "@/components/modal/registration/provider";
 import LoginForm from "@/components/registration/login-form";
-import { useRouter } from "next/navigation";
 
 export default function LoginModal() {
-  const router = useRouter();
   const modal = useModal();
 
-  /*
-  useEffect(() => {
-    setData((prev) => ({
-      ...prev
-    }));
-  }, [data.name]);
-*/
   return (
     <LoginForm isModal={true} onCloseAction={modal?.hide} />
   );
