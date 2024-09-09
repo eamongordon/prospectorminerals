65ae665fb0d89b3b2d549f63dcf3a0ff3ac83269
Eamon Gordon <ekeokigordon@icloud.com>
2024-09-09 07:52:20 -0700

    fix: Revert "refactor: use resolvedTheme for toaster theme"
    This reverts commit df625e35c6753661952e32546cb8d9299f41035e.

--- app/providers.tsx
+++ app/providers.tsx
@@ -6,16 +6,15 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { useRouter } from 'next/navigation';
 import { Toaster } from "sonner";
 import { workSansClassName } from '@/lib/utils';
-import { useTheme } from 'next-themes';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const router = useRouter();
-  const { resolvedTheme } = useTheme();
   return (
     <SessionProvider>
       <NextUIProvider navigate={router.push}>
         <NextThemesProvider attribute="class">
-          <Toaster theme={resolvedTheme as "system" | "light" | "dark"} className={workSansClassName} />
+          <Toaster className={`${workSansClassName} dark:hidden`} />
+          <Toaster theme="dark" className={`${workSansClassName} hidden dark:block`} />
           {children}
         </NextThemesProvider>
       </NextUIProvider>
