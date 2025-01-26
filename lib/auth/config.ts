700c6c621b55dd1d627b94e74a5a385aff845b05
Eamon Gordon <ekeokigordon@icloud.com>
2025-01-26 11:40:22 -0800

    refactor: use import alias for baseUrl in auth config

--- lib/auth/config.ts
+++ lib/auth/config.ts
@@ -3,7 +3,7 @@ import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@/lib/prisma";
-import { baseUrl } from "../utils";
+import { baseUrl } from "@/lib/utils";
 import { JWT } from "next-auth/jwt";
 
 declare module "next-auth" {
