325740df8d9156810c26a796281aaa7521be353f
Eamon Gordon <ekeokigordon@icloud.com>
2025-01-26 11:38:10 -0800

    refactor: update JWT import auth config to use type syntax

--- lib/auth/config.ts
+++ lib/auth/config.ts
@@ -4,7 +4,7 @@ import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@/lib/prisma";
 import { baseUrl } from "../utils";
-import { JWT } from "next-auth/jwt";
+import { type JWT } from "next-auth/jwt";
 
 declare module "next-auth" {
     interface User {
