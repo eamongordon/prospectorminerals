b503bb47bd1c913d4dd2c7a2ceb73c0122cd40a9
Eamon Gordon <ekeokigordon@icloud.com>
2025-02-24 20:07:58 -0800

    fix: remove unnecessary value prop from SelectItem in SortDropdown

--- components/sort-dropdown.tsx
+++ components/sort-dropdown.tsx
@@ -66,7 +66,7 @@ export default function SortDropdown({ sort, sortOptions }: { sort?: string, sor
                 }
             >
                 {(sortOptions || defaultSortOptions).map((item) => (
-                    <SelectItem key={item.value} value={item.value}>
+                    <SelectItem key={item.value}>
                         {item.label}
                     </SelectItem>
                 ))}
